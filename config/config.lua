ece707127c0f33c66b90cd5cef2a58b2bb8dfe5a
unexist <none@none>
2007-01-31 20:02:38 +0100

    [svn] Autotools
    --HG--
    branch : trunk

--- config/config.lua
+++ config/config.lua
@@ -15,8 +15,18 @@ colors = {
 	font				= "#ffffff",						-- Color of the font
 	border			= "#ffffff",						-- Color of the border/tiles
 	normal			= "#CFDCE6",						-- Color of the inactive windows
-	active			= "#6096BF",						-- Color of the active window
+	focus				= "#6096BF",						-- Color of the focussed window
+	shade				= "#bac5ce",						-- Color of shaded windows
 	background	= "#596F80"							-- Color of the root background
+}
+
+colors1 = {
+  font        = "#ffffff",            -- Color of the font
+  border      = "#ffffff",            -- Color of the border/tiles
+  normal      = "#848684",            -- Color of the inactive windows
+  focus	      = "#63659c",            -- Color of focussed window
+	shade				= "#FFE6E6",						-- Color of shaded windows
+  background  = "#adaead"  						-- Color of the root background
 }
 
 -- Key config
