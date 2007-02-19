23ae78a87ef87ad98fa4c88d84e51404403ec6b0
unexist <none@none>
2007-02-19 04:23:02 +0100

    [svn] Added comments, moved the render function to sublet.c, changed some function declaration to reflect the changes
    --HG--
    branch : trunk

--- src/sublet.c
+++ src/sublet.c
@@ -2,12 +2,23 @@
 
 static SubSubletList *list = NULL;
 
+ /**
+	* Find a sublet via the Xlib content manager.
+	* @param win A window
+	* @return Return the #SubSublet associated with the window or NULL
+	**/
+
 SubSublet *
 subSubletFind(Window win)
 {
 	SubSublet *s = NULL;
 	return(XFindContext(d->dpy, win, 3, (void *)&s) != XCNOENT ? s : NULL);
 }
+
+ /**
+	* Sift the sublet through the list
+	* @param pos Position of the sublet in the list
+	**/
 
 void
 subSubletSift(int pos)
@@ -26,11 +37,20 @@ subSubletSift(int pos)
 		}
 }
 
+ /**
+	* Get the recent sublet
+	* @return Return either the recent sublet or NULL if the list is empty
+	**/
+
 SubSublet *
 subSubletGetRecent(void)
 {
 	return(list->size > 0 ? list->content[1] : NULL);
 }
+
+ /**
+	* Init sublet list
+	**/
 
 void
 subSubletNew(void)
@@ -41,8 +61,17 @@ subSubletNew(void)
 	if(!list->content) subLogError("Can't alloc memory. Exhausted?\n");
 }
 
+ /**
+	* Add a new sublet
+	* @param type Type of the sublet
+	* @param ref Lua object reference
+	* @param interval Update interval
+	* @param width Width of the sublet
+	**/
+
 void
-subSubletAdd(const char *function,
+subSubletAdd(int type,
+	int ref,
 	unsigned int interval,
 	unsigned int width)
 {
@@ -62,30 +91,63 @@ subSubletAdd(const char *function,
 	XMoveResizeWindow(d->dpy, screen->statusbar,
 		DisplayWidth(d->dpy, DefaultScreen(d->dpy)) - total_width, 0, total_width, d->th);
 
-	s->time			= subEventGetTime();
+	s->type			= type;
+	s->ref			= ref;
 	s->interval = interval;
-	s->function	= function;
+	s->width		= width * d->fx;
+	s->time			= subEventGetTime();
 	s->win			= SUBWINNEW(screen->statusbar, total_width - width * d->fx, 0, width * d->fx, d->th, 0);
-	
-	subLuaCall(s->function, &s->data);
+
+	subLuaCall(s->ref, &s->data);
 	XSaveContext(d->dpy, s->win, 3, (void *)s);
 
-	list->content = (SubSublet **)realloc(list->content, sizeof(SubSublet *) * (list->size + 2));
-	if(!list->content) subLogError("Can't alloc memory. Exhausted?\n");
-	
-	i = ++(list->size);
-
-	while(i > 1 && s->time < list->content[i / 2]->time)
+	/* Don't add teaser sublets to the list */
+	if(type > SUB_SUBLET_TEXT)
 		{
-			list->content[i] = list->content[i / 2];
-			i /= 2;
+			list->content = (SubSublet **)realloc(list->content, sizeof(SubSublet *) * (list->size + 2));
+			if(!list->content) subLogError("Can't alloc memory. Exhausted?\n");
+	
+			i = ++(list->size);
+
+			while(i > 1 && s->time < list->content[i / 2]->time)
+				{
+					list->content[i] = list->content[i / 2];
+					i /= 2;
+				}
+			list->content[i] = s;
 		}
-	list->content[i] = s;
 
 	XMapRaised(d->dpy, s->win);
-
-	printf("Loaded sublet %s (%d)\n", function, interval);
 }
+
+ /**
+	* Render a sublet.
+	* @param s A #SubSublet
+	**/
+
+void
+subSubletRender(SubSublet *s)
+{
+	if(s && (s->data.string || s->data.number))
+		{
+			XClearWindow(d->dpy, s->win);
+			switch(s->type)
+				{
+					case SUB_SUBLET_TEXT:
+					case SUB_SUBLET_TEASER:
+						XDrawString(d->dpy, s->win, d->gcs.font, 3, d->fy - 1, s->data.string, strlen(s->data.string));
+						break;
+					case SUB_SUBLET_METER:
+						XDrawRectangle(d->dpy, s->win, d->gcs.font, 2, 2, s->width - 4, d->th - 5);
+						XFillRectangle(d->dpy, s->win, d->gcs.font, 4, 4, ((s->width - 7) * s->data.number) / 100, d->th - 8);
+				}
+			XFlush(d->dpy);
+		}
+}
+
+ /**
+	* Kill all sublets 
+	**/
 
 void
 subSubletKill(void)
