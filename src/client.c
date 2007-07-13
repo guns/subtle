52a1919e66baac9d9f8d0bd0fc45548ad19fc0f7
unexist <none@none>
2007-07-13 18:37:00 +0200

    [svn] Transient/urgent windows now raise, added more ICCCM / EWMH stuff
    --HG--
    branch : trunk

--- src/client.c
+++ src/client.c
@@ -20,6 +20,9 @@
 
 #include "subtle.h"
 
+static int size = 0;
+static Window *clients = NULL;
+
  /**
 	* Create a new client
 	* @param win Window of the client
@@ -44,13 +47,21 @@ subClientNew(Window win)
 	w->client->cmap	= attr.colormap;
 	w->flags				= SUB_WIN_TYPE_CLIENT; 
 
+	/* EWMH: Client list and client list stacking */
+	clients = (Window *)realloc(clients, sizeof(Window) * (size + 1));
+	if(!clients) subLogError("Can't alloc memory. Exhausted?\n");
+	clients[size] = w->win;
+	size++;
+	subEwmhSetWindows(DefaultRootWindow(d->dpy), SUB_EWMH_NET_CLIENT_LIST, clients, size);
+	subEwmhSetWindows(DefaultRootWindow(d->dpy), SUB_EWMH_NET_CLIENT_LIST_STACKING, clients, size);
+
 	/* Window caption */
 	XFetchName(d->dpy, win, &w->client->name);
 	if(!w->client->name) w->client->name = strdup("subtle");
 	w->client->caption = XCreateSimpleWindow(d->dpy, w->frame, d->th, 0, 
 		(strlen(w->client->name) + 1) * d->fx, d->th, 0, d->colors.border, d->colors.norm);
 
-	/* Hints */
+	/* Window manager hints */
 	hints = XGetWMHints(d->dpy, win);
 	if(hints)
 		{
@@ -58,12 +69,7 @@ subClientNew(Window win)
 			else subClientSetWMState(w, NormalState);
 			if(hints->initial_state == IconicState) subLogDebug("Iconic: win=%#lx\n", win);			
 			if(hints->input) w->flags |= SUB_WIN_PREF_INPUT;
-			if(hints->flags & (XUrgencyHint|WindowGroupHint)) 
-				{
-					subWinToggle(SUB_WIN_OPT_RAISE, w);
-					printf("Transient: flags=%s%s\n", hints->flags & XUrgencyHint ? "u" : "", 
-						hints->flags & WindowGroupHint ? "g" : "");
-				}
+			if(hints->flags & XUrgencyHint) w->flags |= SUB_WIN_OPT_TRANS;
 			XFree(hints);
 		}
 	
@@ -82,9 +88,7 @@ subClientNew(Window win)
 
 	/* Check for dialog windows etc. */
 	XGetTransientForHint(d->dpy, win, &unnused);
-	if(unnused) subWinToggle(SUB_WIN_OPT_RAISE, w);
-
-	printf("Transient: window=%d\n", unnused);
+	if(unnused && !(w->flags & SUB_WIN_OPT_TRANS)) w->flags |= SUB_WIN_OPT_TRANS;
 
 	XSync(d->dpy, False);
 	XUngrabServer(d->dpy);
@@ -99,13 +103,115 @@ subClientNew(Window win)
 void
 subClientDelete(SubWin *w)
 {
-	if(w->flags & SUB_WIN_TYPE_CLIENT)
+	if(w && w->flags & SUB_WIN_TYPE_CLIENT)
 		{
+			int i, j;
+
+			if(w->flags & SUB_WIN_PREF_CLOSE)
+				{
+					XEvent ev;
+
+					ev.type									= ClientMessage;
+					ev.xclient.window				= w->win;
+					ev.xclient.message_type = subEwmhGetAtom(SUB_EWMH_WM_PROTOCOLS);
+					ev.xclient.format				= 32;
+					ev.xclient.data.l[0]		= subEwmhGetAtom(SUB_EWMH_WM_DELETE_WINDOW);
+					ev.xclient.data.l[1]		= CurrentTime;
+
+					XSendEvent(d->dpy, w->win, False, NoEventMask, &ev);
+
+					printf("Delete: send %#lx delete event\n", w->win);
+				}
+			else XDestroyWindow(d->dpy, w->win);
+
 			if(w->client->name) XFree(w->client->name);
 			free(w->client);
+
+			/* Update client list */
+			for(i = 0; i < size; i++) 	
+				if(clients[i] == w->win)
+					{
+						for(j = i; j < size - 1; j++) clients[j] = clients[j + 1];
+						break; /* Leave loop */
+					}
+
+			clients = (Window *)realloc(clients, sizeof(Window) * size);
+			if(!clients)  subLogError("Can't alloc memory. Exhausted?\n");
+			size--;
+
+			/* EWMH: Client list and client list stacking */			
+			subEwmhSetWindows(DefaultRootWindow(d->dpy), SUB_EWMH_NET_CLIENT_LIST, clients, size);
+			subEwmhSetWindows(DefaultRootWindow(d->dpy), SUB_EWMH_NET_CLIENT_LIST_STACKING, clients, size);					
 		}
 }
 
+ /**
+	* Render the client window
+	* @param w A #SubWin
+	**/
+
+void
+subClientRender(SubWin *w)
+{
+	if(w && w->flags & SUB_WIN_TYPE_CLIENT)
+		{
+			unsigned long col = d->focus && d->focus == w ? d->colors.focus : 
+				(w->flags & SUB_WIN_OPT_COLLAPSE ? d->colors.cover : d->colors.norm);
+
+			XSetWindowBackground(d->dpy, w->client->caption, col);
+			XClearWindow(d->dpy, w->client->caption);
+			XDrawString(d->dpy, w->client->caption, d->gcs.font, 3, d->fy - 1, w->client->name, 
+				strlen(w->client->name));
+		}
+}
+
+ /**
+	* Send a configure request to the client
+	* @param w A #SubWin
+	**/
+
+void
+subClientConfigure(SubWin *w)
+{
+	XConfigureEvent ev;
+
+	ev.type								= ConfigureNotify;
+	ev.event							= w->win;
+	ev.window							= w->win;
+	ev.x									= w->x;
+	ev.y									= w->y;
+	ev.width							= SUBWINWIDTH(w);
+	ev.height							= SUBWINHEIGHT(w);
+	ev.above							= None;
+	ev.border_width 			= 0;
+	ev.override_redirect	= 0;
+
+	XSendEvent(d->dpy, w->win, False, StructureNotifyMask, (XEvent *)&ev);
+}
+
+	/**
+	 * Fetch client name
+	 * @param w A #SubWin
+	 **/
+
+void
+subClientFetchName(SubWin *w)
+{
+	if(w && w->flags & SUB_WIN_TYPE_CLIENT)
+		{
+			int width, mode;
+			if(w->client->name) XFree(w->client->name);
+			XFetchName(d->dpy, w->win, &w->client->name);
+
+			/* Check max length of the caption */
+			width = (strlen(w->client->name) + 1) * d->fx;
+			if(width > w->width - d->th - 4) width = w->width - d->th - 14;
+			XMoveResizeWindow(d->dpy, w->client->caption, d->th, 0, width, d->th);
+
+			mode = (d->focus && d->focus->frame == w->frame) ? 0 : 1;
+			subClientRender(w);
+		}
+}
 
  /**
 	* Set the WM state for the client
@@ -147,101 +253,4 @@ subClientGetWMState(SubWin *w)
 			XFree(data);
 		}
 	return(state);
-}
-
- /**
-	* Send a configure request to the client
-	* @param w A #SubWin
-	**/
-
-void
-subClientSendConfigure(SubWin *w)
-{
-	XConfigureEvent ev;
-
-	ev.type								= ConfigureNotify;
-	ev.event							= w->win;
-	ev.window							= w->win;
-	ev.x									= w->x;
-	ev.y									= w->y;
-	ev.width							= SUBWINWIDTH(w);
-	ev.height							= SUBWINHEIGHT(w);
-	ev.above							= None;
-	ev.border_width 			= 0;
-	ev.override_redirect	= 0;
-
-	XSendEvent(d->dpy, w->win, False, StructureNotifyMask, (XEvent *)&ev);
-}
-
- /**
-	* Send delete event to client if supported, otherwise just kill the window
-	* @param w A #SubWin
-	**/
-
-void
-subClientSendDelete(SubWin *w)
-{
-	if(w && w->flags & SUB_WIN_TYPE_CLIENT)
-		{
-
-			subWinUnmap(w);
-			if(w->flags & SUB_WIN_PREF_CLOSE)
-				{
-					XEvent ev;
-
-					ev.type									= ClientMessage;
-					ev.xclient.window				= w->win;
-					ev.xclient.message_type = subEwmhGetAtom(SUB_EWMH_WM_PROTOCOLS);
-					ev.xclient.format				= 32;
-					ev.xclient.data.l[0]		= subEwmhGetAtom(SUB_EWMH_WM_DELETE_WINDOW);
-					ev.xclient.data.l[1]		= CurrentTime;
-
-					XSendEvent(d->dpy, w->win, False, NoEventMask, &ev);
-				}
-			else subClientDelete(w);
-		}
-}
-
-	/**
-	 * Fetch client name
-	 * @param w A #SubWin
-	 **/
-
-void
-subClientFetchName(SubWin *w)
-{
-	if(w && w->flags & SUB_WIN_TYPE_CLIENT)
-		{
-			int width, mode;
-			if(w->client->name) XFree(w->client->name);
-			XFetchName(d->dpy, w->win, &w->client->name);
-
-			/* Check max length of the caption */
-			width = (strlen(w->client->name) + 1) * d->fx;
-			if(width > w->width - d->th - 4) width = w->width - d->th - 14;
-			XMoveResizeWindow(d->dpy, w->client->caption, d->th, 0, width, d->th);
-
-			mode = (d->focus && d->focus->frame == w->frame) ? 0 : 1;
-			subClientRender(w);
-		}
-}
-
- /**
-	* Render the client window
-	* @param w A #SubWin
-	**/
-
-void
-subClientRender(SubWin *w)
-{
-	if(w && w->flags & SUB_WIN_TYPE_CLIENT)
-		{
-			unsigned long col = d->focus && d->focus == w ? d->colors.focus : 
-				(w->flags & SUB_WIN_OPT_COLLAPSE ? d->colors.cover : d->colors.norm);
-
-			XSetWindowBackground(d->dpy, w->client->caption, col);
-			XClearWindow(d->dpy, w->client->caption);
-			XDrawString(d->dpy, w->client->caption, d->gcs.font, 3, d->fy - 1, w->client->name, 
-				strlen(w->client->name));
-		}
 }
