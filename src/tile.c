3e288c5205717bda4a47089e62c2a3059b732df4
unexist <none@none>
2007-09-17 19:19:51 +0200

    [svn] Removed titlebar and buttons from tiles
    --HG--
    branch : trunk

--- src/tile.c
+++ src/tile.c
@@ -3,19 +3,7 @@
 	* subtle - window manager
 	* Copyright (c) 2005-2007 Christoph Kappel
 	*
-	* This program is free software; you can redistribute it and/or modify
-	* it under the terms of the GNU General Public License as published by
-	* the Free Software Foundation; either version 2 of the License, or
-	* (at your option) any later version.
-	*
-	* This program is distributed in the hope that it will be useful,
-	* but WITHOUT ANY WARRANTY; without even the implied warranty of
-	* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	* GNU General Public License for more details.
-	*
-	* You should have received a copy of the GNU General Public License along
-	* with this program; if not, write to the Free Software Foundation, Inc.,
-	* 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+	* See the COPYING file for the license in the latest tarball.
 	**/
 
 #include "subtle.h"
@@ -29,20 +17,15 @@
 SubWin *
 subTileNew(short mode)
 {
-	SubWin *w = subWinNew(0);
+	SubWin *w = subWinNew();
 
 	w->flags	= SUB_WIN_TYPE_TILE;
 	w->flags |= mode;
-	w->tile		= (SubTile *)calloc(1, sizeof(SubTile));
-	if(!w->tile) subLogError("Can't alloc memory. Exhausted?\n");
-
-	w->tile->btnew	= XCreateSimpleWindow(d->dpy, w->frame, 
-		d->th, 0, 7 * d->fx, d->th, 0, d->colors.border, d->colors.norm);
-	w->tile->btdel	= XCreateSimpleWindow(d->dpy, w->frame, 
-		d->th + 7 * d->fx, 0, 7 * d->fx, d->th, 0, d->colors.border, d->colors.norm);
+	w->tile		= (SubTile *)subUtilAlloc(1, sizeof(SubTile));
 
 	subWinMap(w);
-	subLogDebug("Adding %s-tile: x=%d, y=%d, width=%d, height=%d\n", 
+
+	subUtilLogDebug("Adding %s-tile: x=%d, y=%d, width=%d, height=%d\n", 
 		(w->flags & SUB_WIN_TILE_HORZ) ? "h" : "v", w->x, w->y, w->width, w->height);
 
 	return(w);
@@ -58,67 +41,37 @@ subTileDelete(SubWin *w)
 {
 	if(w && w->flags & SUB_WIN_TYPE_TILE)
 		{
-			unsigned int n, i;
-			Window nil, *wins = NULL;
-
-			XQueryTree(d->dpy, w->win, &nil, &nil, &wins, &n);	
-			for(i = 0; i < n; i++)
+			SubWin *c = w->tile->first;
+			while(c)
 				{
-					SubWin *c = subWinFind(wins[i]);
-					if(c) subWinDelete(c);
+					subWinDelete(c);
+					c = c->next;
 				}
 
-			subLogDebug("Deleting %s-tile with %d children\n", (w->flags & SUB_WIN_TILE_HORZ) ? "h" : "v", n);
+			subUtilLogDebug("Deleting %s-tile\n", (w->flags & SUB_WIN_TILE_HORZ) ? "h" : "v");
 
 			free(w->tile); 
-			XFree(wins);
-		}
-}
-
- /**
-	* Render a tile window 
-	* @param w A #SubWin
-	**/
-
-void
-subTileRender(SubWin *w)
-{
-	if(w && w->flags & SUB_WIN_TYPE_TILE)
-		{
-			unsigned long col = d->focus && d->focus == w ? d->colors.focus : 
-				(w->flags & SUB_WIN_STATE_COLLAPSE ? d->colors.cover : d->colors.norm);
-
-			XSetWindowBackground(d->dpy, w->tile->btnew, col);
-			XSetWindowBackground(d->dpy, w->tile->btdel, col);
-			XClearWindow(d->dpy, w->tile->btnew);
-			XClearWindow(d->dpy, w->tile->btdel);
-
-			/* Descriptive buttons */
-			XDrawString(d->dpy, w->tile->btnew, d->gcs.font, 3, d->fy - 1, 
-				(w->flags & SUB_WIN_TILE_HORZ ? "Newrow" : "Newcol"), 6);
-			XDrawString(d->dpy, w->tile->btdel, d->gcs.font, 3, d->fy - 1, 
-				(w->parent && w->parent->flags & SUB_WIN_TILE_HORZ ? "Delrow" : "Delcol"), 6);
 		}
 }
 
  /**
 	* Add a window to a tile 
-	* @param t A #SubWin
+	* @param p A #SubWin
 	* @param w A #SubWin
 	**/
 
 void
-subTileAdd(SubWin *t,
+subTileAdd(SubWin *p,
 	SubWin *w)
 {
-	if(t && w)
+	if(p && w)
 		{
-			XReparentWindow(d->dpy, w->frame, t->win, 0, 0);
-			subWinRestack(w);
-			subTileConfigure(t);
-			w->parent = t;
+			subWinAppend(p, w);
 
-			subLogDebug("Adding window: x=%d, y=%d, width=%d, height=%d\n", w->x, w->y, w->width, w->height);
+			XReparentWindow(d->dpy, w->frame, p->frame, 0, 0);
+
+			subTileConfigure(p);
+			subUtilLogDebug("Adding window: x=%d, y=%d, width=%d, height=%d\n", w->x, w->y, w->width, w->height);
 		}
 }
 
@@ -132,36 +85,49 @@ subTileConfigure(SubWin *w)
 {
 	if(w && w->flags & SUB_WIN_TYPE_TILE)
 		{
-			int mw = 0, mh = 0, width = SUBWINWIDTH(w), height = SUBWINHEIGHT(w);
-			unsigned int x = 0, y = 0, i = 0, n = 0, size = 0, comp = 0, collapsed = 0, weighted = 0, full = 0;
-			Window nil, *wins = NULL;
+			SubWin *c = w->tile->first;
+			int mw = 0, mh = 0, width = w->width, height = w->height;
+			unsigned int x = 0, y = 0, n = 0, size = 0, comp = 0, collapsed = 0, weighted = 0, full = 0, raised = 0;
 
-			XQueryTree(d->dpy, w->win, &nil, &nil, &wins, &n);
-			if(n > 0)
+			if(c)
 				{
 					size = (w->flags & SUB_WIN_TILE_HORZ) ? width : height;
 
-					/* Find weighted and collapsed windows */
-					for(i = 0; i < n; i++)
+					/* Find special windows */
+					while(c)
 						{
-							SubWin *c = subWinFind(wins[i]);
-							if(c)
+							if(c->flags & SUB_WIN_STATE_COLLAPSE) collapsed++;
+							else if(c->flags & SUB_WIN_STATE_FULL) full++;
+							else if(c->flags & SUB_WIN_STATE_RAISE) raised++;
+							else if(c->flags & SUB_WIN_STATE_WEIGHT && c->weight > 0)
 								{
-									if(c->flags & SUB_WIN_STATE_COLLAPSE) collapsed++;
-									else if(c->flags & SUB_WIN_STATE_FULL) full++;
-									else if(c->flags & SUB_WIN_STATE_WEIGHT && c->weight > 0)
+									/* Prevent only weighted windows */
+									if(!c->next && weighted == n)
 										{
-											/* Prevent weighted single or only weighted windows */
-											if(n == 1 || (i == n - 1 && weighted == n - 1))
-												{
-													c->flags &= ~SUB_WIN_STATE_WEIGHT;
-													c->weight = 0;
-													continue;
-												}
-											else size -= width * c->weight / 100;
+											c->flags &= ~SUB_WIN_STATE_WEIGHT;
+											c->weight = 0;
+										}
+									else 
+										{
+											size -= width * c->weight / 100;
 											weighted++;
 										}
 								}
+							c = c->next;
+							n++;
+						}
+
+					/* Remove unneeded tiles */
+					if(n == 1 && !(w->flags & SUB_WIN_TYPE_SCREEN))
+						{
+							SubWin *swap = w->tile->first;
+							subWinReplace(w, swap);
+							XReparentWindow(d->dpy, swap->frame, swap->parent->frame, 0, 0);
+							subWinCut(w);
+							w->tile->first = NULL;
+							subWinDelete(w);
+							printf("Removing dynamic window..\n");
+							return;
 						}
 
 					/* Piled window */
@@ -177,7 +143,7 @@ subTileConfigure(SubWin *w)
 						}
 
 					 /* Prevent divide by zero */
-					n		= (n - collapsed - weighted - full) > 0 ? n - collapsed - weighted - full: 1;
+					n		= (n - collapsed - weighted - full - raised) > 0 ? n - collapsed - weighted - full - raised : 1;
 					mw	= (w->flags & SUB_WIN_TILE_HORZ) ? width / n : width;
 					mh	= (w->flags & SUB_WIN_TILE_VERT) ? (height - collapsed * d->th) / n : height;
 
@@ -185,18 +151,18 @@ subTileConfigure(SubWin *w)
 					if(w->flags & SUB_WIN_TILE_HORZ) comp = abs(width - n * mw - collapsed * d->th);
 					else comp = abs(height - n * mh - collapsed * d->th);
 
-					for(i = 0; i < n + collapsed + weighted + full; i++)
+					c = w->tile->first;
+					while(c)
 						{
-							SubWin *c = subWinFind(wins[i]);
-							if(c && !(c->flags & (SUB_WIN_STATE_TRANS|SUB_WIN_STATE_RAISE|SUB_WIN_STATE_FULL)))
+							if(!(c->flags & (SUB_WIN_STATE_TRANS|SUB_WIN_STATE_RAISE|SUB_WIN_STATE_FULL)))
 								{
 									if(w->flags & SUB_WIN_STATE_PILE && w->tile->pile != c) c->flags |= SUB_WIN_STATE_COLLAPSE;
 
 									c->parent	= w;
-									c->x			= 0;
+									c->x			= w->flags & SUB_WIN_TYPE_SCREEN ? d->th : 0;
 									c->y			= (c->flags & SUB_WIN_STATE_COLLAPSE) ? y : collapsed * d->th;
 									c->width	= mw;
-									c->height = (c->flags & SUB_WIN_STATE_COLLAPSE) ? d->th : mh;
+									c->height	= (c->flags & SUB_WIN_STATE_COLLAPSE) ? d->th : mh;
 
 									/* Adjust sizes according to the tile alignment */
 									if(w->flags & SUB_WIN_TILE_HORZ)
@@ -214,7 +180,7 @@ subTileConfigure(SubWin *w)
 										}
 
 									/* Add compensation to width or height */
-									if(i == n + collapsed + weighted - 1) 
+									if(w->tile->last == c) 
 										if(w->flags & SUB_WIN_TILE_HORZ) c->width += comp; 
 										else c->height += comp;
 		
@@ -226,7 +192,7 @@ subTileConfigure(SubWin *w)
 										}
 									if(w->flags & SUB_WIN_TILE_VERT) y += c->height;
 
-									subLogDebug("Configuring %s-window: x=%d, y=%d, width=%d, height=%d, weight=%d\n", 
+									subUtilLogDebug("Configuring %s-window: x=%d, y=%d, width=%d, height=%d, weight=%d\n", 
 										(c->flags & SUB_WIN_STATE_COLLAPSE) ? "c" : ((w->flags & SUB_WIN_STATE_WEIGHT) ? "w" : 
 										((w->flags & SUB_WIN_STATE_RAISE) ? "r" : "n")), c->x, c->y, c->width, c->height, c->weight);
 
@@ -234,9 +200,9 @@ subTileConfigure(SubWin *w)
 									if(w->flags & SUB_WIN_TYPE_TILE) subTileConfigure(c);
 									else if(w->flags & SUB_WIN_TYPE_CLIENT) subClientConfigure(c);
 								}
+							c = c->next;
 						}
-					XFree(wins);
-					subLogDebug("Configuring %s-tile: n=%d, mw=%d, mh=%d\n", (w->flags & SUB_WIN_TILE_HORZ) ? "h" : "v", n, mw, mh);
+					subUtilLogDebug("Configuring %s-tile: n=%d, mw=%d, mh=%d\n", (w->flags & SUB_WIN_TILE_HORZ) ? "h" : "v", n, mw, mh);
 				}
 		}
 }
