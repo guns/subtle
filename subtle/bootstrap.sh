16e8e9f60bfe9e9b1b2698cc6fa00309a9335a2e
unexist <none@none>
2007-09-22 16:53:54 +0200

    [svn] Added svn revision
    --HG--
    branch : trunk

--- subtle/bootstrap.sh
+++ subtle/bootstrap.sh
@@ -1,5 +1,14 @@
 #! /bin/bash
 set -x
+
+ver="0.7.1"
+rev=`svn info|sed -n 's/Revision: \([0-9]*\)/\1/p'`
+if test -z "$rev"; then
+	$rev="UNKNOWN"
+fi
+
+sed -i -e 's/AC_INIT(subtle, \(.*\))/AC_INIT(subtle, '$ver'-r'$rev')/' configure.in
+
 aclocal
 autoheader
 automake --add-missing --copy
