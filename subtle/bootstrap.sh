efd97324214be159e1d7bbb7a993160a0a16e05d
unexist <none@none>
2007-09-23 16:20:39 +0200

    [svn] Fixture for the revision number
    --HG--
    branch : trunk

--- subtle/bootstrap.sh
+++ subtle/bootstrap.sh
@@ -1,13 +1,12 @@
 #! /bin/bash
 set -x
 
-ver="0.7.1"
-rev=`svn info|sed -n 's/Revision: \([0-9]*\)/\1/p'`
+rev=`svn info | sed -n 's/Revision: \([0-9]*\)/\1/p'`
 if test -z "$rev"; then
 	$rev="UNKNOWN"
 fi
 
-sed -i -e 's/AC_INIT(subtle, \(.*\))/AC_INIT(subtle, '$ver'-r'$rev')/' configure.in
+sed -i -e 's/AC_INIT(\(.*\), \([0-9\.]*\)[-r0-9]*, \(.*\))/AC_INIT(\1, \2-r'$rev', \3)/' configure.in
 
 aclocal
 autoheader
