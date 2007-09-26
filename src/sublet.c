ab1b8930d487d5463b126295fda6aa4fc98fd98c
unexist <none@none>
2007-09-26 23:35:07 +0200

    [svn] Changed sublet subsystem to support a new sublet type and dynamic width
    --HG--
    branch : trunk

--- src/sublet.c
+++ src/sublet.c
@@ -10,23 +10,24 @@
 
 static int size = 0;
 static SubSublet **sublets = NULL;
+static SubSublet *first = NULL;
 
  /**
 	* Find a window via Xlib context manager 
-	* @param win A #Window
-	* @return Return the #SubSublet associated with the window or NULL
+	* @param win A watch descriptor
+	* @return Return the #SubSublet associated with the wd or NULL
 	**/
 
 SubSublet *
-subSubletFind(Window win)
+subSubletFind(int wd)
 {
 	SubSublet *s = NULL;
-	return(XFindContext(d->dpy, win, 2, (void *)&s) != XCNOENT ? s : NULL);
+	return(XFindContext(d->dpy, d->bar.sublets, wd, (void *)&s) != XCNOENT ? s : NULL);
 }
 
  /**
-	* Sift the sublet through the sublets 
-	* @param pos Position of the sublet in the sublets
+	* Sift the sublet through the queue
+	* @param pos Position of the sublet in the queue
 	**/
 
 void
@@ -47,8 +48,8 @@ subSubletSift(int pos)
 }
 
  /**
-	* Get the next sublet 
-	* @return Return either the next #SubSublet or NULL if the sublets is empty
+	* Get next sublet 
+	* @return Return either the next #SubSublet or NULL if no sublets are loaded
 	**/
 
 SubSublet *
@@ -62,37 +63,49 @@ subSubletNext(void)
 	* @param type Type of the sublet
 	* @param ref Lua object reference
 	* @param interval Update interval
-	* @param width Width of the sublet
+	* @param watch Watch file
 	**/
 
 void
 subSubletNew(int type,
 	int ref,
 	time_t interval,
-	unsigned int width)
+	char *watch)
 {
 	int i;
 	SubSublet *s = (SubSublet *)subUtilAlloc(1, sizeof(SubSublet));
 
-	if(!sublets) sublets = (SubSublet **)subUtilAlloc(1, sizeof(SubSublet *));
+	if(!sublets) 
+		{
+			first		= s;
+			sublets = (SubSublet **)subUtilAlloc(1, sizeof(SubSublet *));
+		}
 
 	/* Init the sublet */
 	s->flags		= type;
 	s->ref			= ref;
-	s->width		= width * d->fx;
 	s->interval	= interval;
 	s->time			= subEventGetTime();
-	s->win			= XCreateSimpleWindow(d->dpy, d->bar.sublets, 0, 0, 1, d->th, 0, 0, d->colors.norm);
+
+#ifdef HAVE_SYS_INOTIFY_H
+	if(s->flags & SUB_SUBLET_TYPE_WATCH && ((s->interval = inotify_add_watch(d->notify, watch, IN_MODIFY)) < 0))
+		{
+			subUtilLogWarn("Can't create inotify watch\n");
+			subUtilLogDebug("Inotify: %s\n", strerror(errno));
+
+			return;
+		}
+	else XSaveContext(d->dpy, d->bar.sublets, s->interval, (void *)s);
+#endif /* HAVE_SYS_INOTIFY_H */
 
 	subLuaCall(s);
-	XSaveContext(d->dpy, s->win, 2, (void *)s);
 
-	/* Don't add text sublets to the sublet list */
-	if(type != SUB_SUBLET_TYPE_TEXT)
-		{
+  /* Don't add text and watch type sublets to the queue */
+  if(type != SUB_SUBLET_TYPE_TEXT && type != SUB_SUBLET_TYPE_WATCH)
+    {
 			sublets = (SubSublet **)realloc(sublets, sizeof(SubSublet *) * (size + 2));
 			if(!sublets) subUtilLogError("Can't alloc memory. Exhausted?\n");
-	
+
 			i = ++size;
 
 			while(i > 1 && s->time < sublets[i / 2]->time)
@@ -102,7 +115,10 @@ subSubletNew(int type,
 				}
 			sublets[i] = s;
 		}
-	XMapRaised(d->dpy, s->win);
+	
+	/* Smart use of unused array index */
+	if(first != s) sublets[0]->next = s;
+	sublets[0] = s;
 }
 
  /**
@@ -116,25 +132,56 @@ subSubletDelete(SubSublet *s)
 }
 
  /**
-	* Render a sublet 
-	* @param s A #SubSublet
+	* Configure sublet bar
 	**/
 
 void
-subSubletRender(SubSublet *s)
+subSubletConfigure(void)
 {
-	if(s)
+	if(sublets)
 		{
-			XClearWindow(d->dpy, s->win);
+			int width = 3;
+			SubSublet *s = first;
 
-			if(s->flags & (SUB_SUBLET_TYPE_TEXT|SUB_SUBLET_TYPE_TEASER) && s->string)
+			while(s)
 				{
-					XDrawString(d->dpy, s->win, d->gcs.font, 3, d->fy - 1, s->string, strlen(s->string));
+					if(s->flags & SUB_SUBLET_TYPE_METER) width += 66;
+					else width += strlen(s->string) * d->fx + 12;
+					s = s->next;
+					printf("width=%d\n", width);
 				}
-			else if(s->flags & SUB_SUBLET_TYPE_METER && s->number)
+			XMoveResizeWindow(d->dpy, d->bar.sublets, DisplayWidth(d->dpy, DefaultScreen(d->dpy)) - width, 0, width, d->th);
+		}
+}
+
+ /**
+	* Render the sublet s
+	**/
+
+void
+subSubletRender(void)
+{
+	if(sublets)
+		{
+			int i, width = 3;
+			SubSublet *s = first;
+
+			XClearWindow(d->dpy, d->bar.sublets);
+
+			while(s)
 				{
-					XDrawRectangle(d->dpy, s->win, d->gcs.font, 2, 2, s->width - 4, d->th - 5);
-					XFillRectangle(d->dpy, s->win, d->gcs.font, 4, 4, ((s->width - 7) * s->number) / 100, d->th - 8);
+					if(s->flags & SUB_SUBLET_TYPE_METER && s->number)
+						{
+							XDrawRectangle(d->dpy, d->bar.sublets, d->gcs.font, width, 2, 60, d->th - 5);
+							XFillRectangle(d->dpy, d->bar.sublets, d->gcs.font, width + 2, 4, (56 * s->number) / 100, d->th - 8);
+							width += 63;
+						}
+					else if(s->string) 
+						{
+							XDrawString(d->dpy, d->bar.sublets, d->gcs.font, width, d->fy - 1, s->string, strlen(s->string));
+							width += strlen(s->string) * d->fx + 6;
+						}
+					s = s->next;
 				}
 			XFlush(d->dpy);
 		}
@@ -147,14 +194,12 @@ subSubletRender(SubSublet *s)
 void
 subSubletKill(void)
 {
-	int i;
+	SubSublet *s = first;
 
-	for(i = 1; i < size; i++)
+	while(s)
 		{
-			XUnmapWindow(d->dpy, sublets[i]->win);
-			XDeleteContext(d->dpy, sublets[i]->win, 2);
-			XDestroyWindow(d->dpy, sublets[i]->win);
-			free(sublets[i]);
+			free(s);
+			s = s->next;
 		}
 	free(sublets);
 }
