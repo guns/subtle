447f53aa35f76ac93cd207b35d3d0fe64c1b305b
unexist@meanas <none@none>
2007-10-01 19:04:10 +0200

    hg tip instead of hg log
    --HG--
    branch : trunk

--- subtle/bootstrap.sh
+++ subtle/bootstrap.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 set -x
-rev=`hg log | head -n1 | sed -n 's/changeset:[ ]*\([0-9]*\):[0-9a-zA-Z]*/\1/p'`
+rev=`hg tip | sed -n 's/changeset:[ ]*\([0-9]*\):[0-9a-zA-Z]*/\1/p'`
 #rev=`svn info | sed -n 's/Revision: \([0-9]*\)/\1/p'`
 if test -z "$rev"; then
 	$rev="UNKNOWN"
