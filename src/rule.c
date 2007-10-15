75a4bbf34821dc906a6d86d23af9b713a0cc05a7
unexist <none@none>
2007-10-15 23:14:43 +0200

    Greatly improved tagging
    --HG--
    branch : trunk

--- src/rule.c
+++ src/rule.c
@@ -8,101 +8,68 @@
 
 #include "subtle.h"
 
-static int nrules = 0;
-static SubRule **rules = NULL;
-
  /**
 	* Find a rule
+	* @param r A #SubRule
 	* @param tag Tag name
-	* @return Return the view associated with the rule or NULL
+	* @return Return either nonzero on match or zero
 	**/
 
-SubRule *
-subRuleFind(char *tag)
+int
+subRuleMatch(SubRule *r,
+	char *tag)
 {
-	int i;
-
-	for(i = 0; i < nrules; i++)
-		if((rules[i]->flags == SUB_RULE_TYPE_REGEX && regexec(rules[i]->regex, tag, 0, NULL, 0)) ||
-			(rules[i]->flags == SUB_RULE_TYPE_VALUE && (!strncasecmp(tag, rules[i]->tag, strlen(rules[i]->tag)))))
-				return(rules[i]);
-	return(NULL);
+	assert(r && tag);
+	return(regexec(r->regex, tag, 0, NULL, 0) ? 1 : 0);
 }
 
  /**
 	* Create new rule
 	* @param tag Tag name
-	* @param v A #SubView
 	**/
 
 SubRule *
-subRuleNew(char *tag,
-	SubView *v)
+subRuleNew(char *tags)
 {
+	int errcode;
 	SubRule *r = NULL;
 
-	assert(tag && v);
+	assert(tags);
 
-	r			= (SubRule *)subUtilAlloc(1, sizeof(SubRule));
-	r->v	= v;
+	r	= (SubRule *)subUtilAlloc(1, sizeof(SubRule));
+	r->regex = (regex_t *)subUtilAlloc(1, sizeof(regex_t));
 
-	if(!rules) rules = (SubRule **)subUtilAlloc(1, sizeof(SubRule *));
-	if(tag[0] == '/')
+	/* Stupid error handling */
+	if((errcode = regcomp(r->regex, tags, REG_EXTENDED|REG_NOSUB|REG_ICASE)))
 		{
-			int errcode;
-			char *buf = (char *)subUtilAlloc(strlen(tag) - 1, sizeof(char));
-			strncpy(buf, tag + 1, strlen(tag) - 2);
+			size_t errsize = regerror(errcode, r->regex, NULL, 0);
+			char *errbuf = (char *)subUtilAlloc(1, errsize);
+			regerror(errcode, r->regex, errbuf, errsize);
 
-			r->flags = SUB_RULE_TYPE_REGEX;
-			r->regex = (regex_t *)subUtilAlloc(1, sizeof(regex_t));
+			subUtilLogWarn("Can't compile regex `%s'\n", tags);
+			subUtilLogDebug("%s\n", errbuf);
 
-			/* Stupid error handling */
-			if((errcode = regcomp(r->regex, buf, REG_EXTENDED|REG_NOSUB|REG_ICASE)))
-				{
-					size_t errsize = regerror(errcode, r->regex, NULL, 0);
-					char *errbuf = (char *)subUtilAlloc(1, errsize);
-					regerror(errcode, r->regex, errbuf, errsize);
-
-					subUtilLogWarn("Can't compile regex `%s'\n", tag);
-					subUtilLogDebug("%s\n", errbuf);
-
-					free(buf);
-					free(errbuf);
-					regfree(r->regex);
-					free(r);
-					return(NULL);
-				}
-			free(buf);
-		}
-	else
-		{
-			r->flags	= SUB_RULE_TYPE_VALUE;
-			r->tag		= strdup(tag);
+			free(errbuf);
+			regfree(r->regex);
+			free(r->regex);
+			free(r);
+			return(NULL);
 		}
 
-	subUtilLogDebug("Rule: view=%s, tag=%s, type=%s\n", v->name, tag, r->flags & SUB_RULE_TYPE_REGEX ? "regex" : "value");
-	rules = (SubRule **)subUtilRealloc(rules, sizeof(SubRule *) * (nrules + 2));
-	rules[nrules++] = r;
+	subUtilLogDebug("Rule: tag=%s\n", tags);
 	return(r);
 }
 
  /**
-	* Kill all rules
+	* Delete rule
+	* @param r A #SubRule
 	**/
 
 void
-subRuleKill(void)
+subRuleDelete(SubRule *r)
 {
-	if(rules)
-		{
-			int i;
-
-			for(i = 0; i < nrules; i++)
-				{
-					if(rules[i]->flags == SUB_RULE_TYPE_REGEX) if(rules[i]->regex) regfree(rules[i]->regex);
-					else if(rules[i]->flags == SUB_RULE_TYPE_VALUE) if(rules[i]->tag) free(rules[i]->tag);
-					free(rules[i]);
-				}
-			free(rules);
-		}
+	assert(r);
+	regfree(r->regex);
+	free(r->regex);
+	free(r);
 }
