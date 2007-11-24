8369bab29f4420ff6e96f47c73a2258d21b82ac1
unexist <none@none>
2007-11-25 00:41:39 +0100

    Added folds and tidied up a bit
    --HG--
    branch : trunk

--- src/subtle.h
+++ src/subtle.h
@@ -18,9 +18,7 @@
 #include <X11/Xatom.h>
 #include <X11/Xmd.h>
 
-#ifdef HAVE_CONFIG_H
 #include "config.h"
-#endif /* HAVE_CONFIG_H */
 
 #ifdef HAVE_SYS_INOTIFY_H
 #include <sys/inotify.h>
@@ -34,24 +32,25 @@
 #define SUBWINWIDTH(w)	(w->width - 2 * d->bw)				// Get real window width
 #define SUBWINHEIGHT(w)	(w->height - d->th - d->bw)		// Get real window height
 
-/* win.c */
-#define SUB_WIN_TYPE_VIEW			(1L << 1)						// View window
-#define SUB_WIN_TYPE_TILE			(1L << 2)						// Tile window
-#define SUB_WIN_TYPE_CLIENT		(1L << 3)						// Client window
+/* win.c {{{ */
+#define SUB_WIN_TYPE_VIEW			(1L << 1)							// View window
+#define SUB_WIN_TYPE_TILE			(1L << 2)							// Tile window
+#define SUB_WIN_TYPE_CLIENT		(1L << 3)							// Client window
 
-#define SUB_WIN_STATE_SHADE		(1L << 4)						// Shaded window
-#define SUB_WIN_STATE_FLOAT		(1L << 5)						// Floated window
-#define SUB_WIN_STATE_FULL		(1L << 6)						// Fullscreen window
-#define SUB_WIN_STATE_RESIZE	(1L << 7)						// Resized window
-#define SUB_WIN_STATE_STACK		(1L << 8)						// Piled tiling window
-#define SUB_WIN_STATE_TRANS		(1L << 9)						// Transient window
+#define SUB_WIN_STATE_SHADE		(1L << 4)							// Shaded window
+#define SUB_WIN_STATE_FLOAT		(1L << 5)							// Floated window
+#define SUB_WIN_STATE_FULL		(1L << 6)							// Fullscreen window
+#define SUB_WIN_STATE_RESIZE	(1L << 7)							// Resized window
+#define SUB_WIN_STATE_STACK		(1L << 8)							// Stacked tiling window
+#define SUB_WIN_STATE_TRANS		(1L << 9)							// Transient window
+#define SUB_WIN_STATE_DEAD		(1L << 10)						// Dead window
 
-#define SUB_WIN_PREF_INPUT		(1L << 10)					// Active/passive focus-model
-#define SUB_WIN_PREF_FOCUS		(1L << 11)					// Send focus message
-#define SUB_WIN_PREF_CLOSE		(1L << 12)					// Send close message
+#define SUB_WIN_PREF_INPUT		(1L << 11)						// Active/passive focus-model
+#define SUB_WIN_PREF_FOCUS		(1L << 12)						// Send focus message
+#define SUB_WIN_PREF_CLOSE		(1L << 13)						// Send close message
 
-#define SUB_WIN_TILE_VERT			(1L << 13)					// Vert-tile
-#define SUB_WIN_TILE_HORZ			(1L << 14)					// Horiz-tile>
+#define SUB_WIN_TILE_VERT			(1L << 14)						// Vert tile
+#define SUB_WIN_TILE_HORZ			(1L << 15)						// Horiz tile>
 
 /* Forward declarations */
 struct subtile;
@@ -78,7 +77,7 @@ SubWin *subWinNew(void);															// Create new window
 void subWinDelete(SubWin *w);													// Delete window
 void subWinDestroy(SubWin *w);												// Destroy window
 void subWinRender(SubWin *w);													// Render wrapper
-void subWinCut(SubWin *w);														// Cut window
+void subWinUnlink(SubWin *w);														// Cut window
 void subWinPrepend(SubWin *w1, SubWin *w2);						// Prepend window
 void subWinAppend(SubWin *w1, SubWin *w2);						// Append window
 void subWinReplace(SubWin *w1, SubWin *w2);						// Replace two windows
@@ -88,20 +87,23 @@ void subWinResize(SubWin *w);													// Resize window
 void subWinReparent(SubWin *p, SubWin *w);						// Reparent window
 void subWinMap(SubWin *w);														// Map window
 void subWinUnmap(SubWin *w);													// Unmap window
+/* }}} */
 
-/* tile.c */
+/* tile.c {{{ */
 typedef struct subtile
 {
+	int cw, ch;																					// Calculated width/height
 	SubWin *pile, *first, *last;												// Pile top, first/last child
 } SubTile;
 
-SubWin *subTileNew(short mode);												// Create new tile
+SubWin *subTileNew(int mode);													// Create new tile
 void subTileDelete(SubWin *t);												// Delete tile
 void subTileDestroy(SubWin *t);												// Destroy tile
 void subTileAdd(SubWin *t, SubWin *w);								// Add a window to tile
 void subTileConfigure(SubWin *t);											// Configure tile
+/* }}} */
 
-/* client.c */
+/* client.c {{{ */
 #define SUB_CLIENT_DRAG_LEFT					(1L << 1)				// Drag start from left
 #define SUB_CLIENT_DRAG_RIGHT					(1L << 2)				// Drag start from right
 #define SUB_CLIENT_DRAG_BOTTOM				(1L << 3)				// Drag start from bottom
@@ -130,16 +132,16 @@ typedef struct subclient
 
 SubWin *subClientNew(Window win);											// Create new client
 void subClientDelete(SubWin *w);											// Delete client
-void subClientDestroy(SubWin *w);											// Destroy client
 void subClientRender(SubWin *w);											// Render the window
 void subClientConfigure(SubWin *w);										// Send configure request
-void subClientDrag(short mode, SubWin *w);						// Move/Resize a window
-void subClientToggle(short type, SubWin *w);					// Toggle various states
+void subClientDrag(int mode, SubWin *w);						// Move/Resize a window
+void subClientToggle(int type, SubWin *w);					// Toggle various states
 void subClientFetchName(SubWin *w);										// Fetch client name
 void subClientSetWMState(SubWin *w, long state);			// Set client WM state
 long subClientGetWMState(SubWin *w);									// Get client WM state
+/* }}} */
 
-/* view.c */
+/* view.c {{{ */
 typedef struct subview
 {
 	int			width, xid;																	// View button width, view xid
@@ -158,12 +160,13 @@ SubView *subViewNew(char *name, char *tags);					// Create new view
 void subViewDelete(SubView *v);												// Delete view
 void subViewDestroy(SubView *v);											// Destroy view
 void subViewKill(void);																// Kill views
-void subViewSift(Window win);													// Sift window
+void subViewMerge(Window win);													// Merge window
 void subViewRender(void);															// Render views
 void subViewConfigure(void);													// Configure views
 void subViewSwitch(SubView *v);												// Switch view
+/* }}} */
 
-/* sublet.c */
+/* sublet.c {{{ */
 #define SUB_SUBLET_TYPE_TEXT		(1L << 1)							// Text sublet
 #define SUB_SUBLET_TYPE_TEASER	(1L << 2)							// Teaser sublet
 #define SUB_SUBLET_TYPE_METER		(1L << 3)							// Meter sublet
@@ -193,11 +196,12 @@ void subSubletDelete(SubSublet *s);										// Delete sublet
 void subSubletDestroy(SubSublet *s);									// Destroy sublet
 void subSubletRender(void);														// Render sublet
 void subSubletConfigure(void);												// Configure sublet bar
-void subSubletSift(int pos);													// Sift sublet
+void subSubletMerge(int pos);													// Merge sublet
 SubSublet *subSubletNext(void);												// Get next sublet
 void subSubletKill(void);															// Delete sublets
+/* }}} */
 
-/* display.c */
+/* display.c {{{ */
 typedef struct subdisplay
 {
 	Display						*disp;														// Connection to X server
@@ -237,8 +241,9 @@ extern SubDisplay *d;
 void subDisplayNew(const char *display_string);				// Create new display
 void subDisplayKill(void);														// Delete display
 void subDisplayScan(void);														// Scan root window
+/* }}} */
 
-/* key.c */
+/* key.c {{{ */
 #define SUB_KEY_ACTION_FOCUS_ABOVE			(1L << 1)			// Focus above window
 #define SUB_KEY_ACTION_FOCUS_BELOW			(1L << 2)			// Focus below window
 #define SUB_KEY_ACTION_FOCUS_NEXT				(1L << 3)			// Focus next window
@@ -276,35 +281,39 @@ void subKeyNew(const char *key,
 void subKeyGrab(SubWin *w);														// Grab keys for a window
 void subKeyUngrab(SubWin *w);													// Ungrab keys for a window
 void subKeyKill(void);																// Delete all keys
+/* }}} */
 
-/* lua.c */
+/* lua.c {{{ */
 void subLuaLoadConfig(const char *path);							// Load config file
 void subLuaLoadSublets(const char *path);							// Load sublets
 void subLuaKill(void);																// Kill Lua state
 void subLuaCall(SubSublet *s);												// Call Lua script
+/* }}} */
 
-/* event.c */
+/* event.c {{{ */
 time_t subEventGetTime(void);													// Get the current time
 int subEventLoop(void);																// Event loop
+/* }}} */
 
-/* util.c */
+/* util.c {{{ */
 #ifdef DEBUG
 void subUtilLogToggle(void);
-#define subUtilLogDebug(...)	subUtilLog(0, __FILE__, __LINE__, __VA_ARGS__);
+#define subUtilLogDebug(...)	subUtilLog(0, __FILE__, __func__, __LINE__, __VA_ARGS__);
 #else
 #define subUtilLogDebug(...)
 #endif /* DEBUG */
 
-#define subUtilLogError(...)	subUtilLog(1, __FILE__, __LINE__, __VA_ARGS__);
-#define subUtilLogWarn(...)		subUtilLog(2, __FILE__, __LINE__, __VA_ARGS__);
+#define subUtilLogError(...)	subUtilLog(1, __FILE__,__func__, __LINE__, __VA_ARGS__);
+#define subUtilLogWarn(...)		subUtilLog(2, __FILE__,__func__, __LINE__, __VA_ARGS__);
 
-void subUtilLog(short type, const char *file, 				// Print messages
-	short line, const char *format, ...);
+void subUtilLog(int type, const char *file, 				// Print messages
+	const char *func, int line, const char *format, ...);
 void *subUtilRealloc(void *mem, size_t size);					// Reallocate memory
 void *subUtilAlloc(size_t n, size_t size);						// Allocate memory
 XPointer *subUtilFind(Window win, XContext id);				// Find window data
+/* }}} */
 
-/* ewmh.c */
+/* ewmh.c  {{{ */
 enum SubEwmhHints
 {
 	/* ICCCM */
@@ -371,5 +380,6 @@ void subEwmhSetString(Window win, int hint,
 	char *value);																					// Set string property
 void subEwmhSetStrings(Window win, int hint,						// Set string properties
 	char **values, int size);
+/* }}} */
 
 #endif /* SUBTLE_H */
