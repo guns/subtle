5610956871478c22c5921be29d4657513516045d
unexist <none@none>
2008-03-01 23:07:40 +0100

    Folding, more doc and function rename
    --HG--
    branch : trunk

--- src/array.c
+++ src/array.c
@@ -20,57 +20,54 @@ SubArray *
 subArrayNew(void)
 {
 	return((SubArray *)subUtilAlloc(1, sizeof(SubArray)));
-}
-/* }}} */
+} /* }}} */
 
- /** sub ArrayDelete {{{
-	* Delete element from array
-	* @param[in] a Array
+ /** subArrayPush {{{
+	* Push element to array
+	* @param[in] a A #SubArray
+	* @param[in] elem New element
+	**/
+
+void
+subArrayPush(SubArray *a,
+	void *elem)
+{
+	assert(a && elem);
+
+	a->data = (void **)subUtilRealloc(a->data, a->ndata + 1 * sizeof(void *));
+	a->data[(a->ndata)++] = elem;
+} /* }}} */
+
+ /** subArrayPop {{{
+	* Pop element from array
+	* @param[in] a #SubArray
 	* @param[in] elem Array element
 	**/
 
 void
-subArrayDelete(SubArray *a,
+subArrayPop(SubArray *a,
 	void *elem)
 {
 	int i, j;
 
 	assert(a && elem);
 
-	for(i = 0; i < a->size; i--)
+	for(i = 0; i < a->ndata; i--)
 	{
 		if(a->data[i] == elem)
 			{
-				for(j = i; j < a->size; j++) a->data[j] = a->data[j + 1];
-				a->size--;
-				a->data = (void **)subUtilRealloc(a->data, a->size * sizeof(void *));
+				for(j = i; j < a->ndata; j++) a->data[j] = a->data[j + 1];
+				a->ndata--;
+				a->data = (void **)subUtilRealloc(a->data, a->ndata * sizeof(void *));
 				return;
 			}
 		}
 
-}
-/* }}} */
-
- /** subArrayAdd {{{
-	* Add element to array
-	* @param[in] a Array
-	* @param[in] elem New array element
-	**/
-
-void
-subArrayAdd(SubArray *a,
-	void *elem)
-{
-	assert(a && elem);
-
-	a->data = (void **)subUtilRealloc(a->data, a->size + 1 * sizeof(void *));
-	a->data[(a->size)++] = elem;
-}
-/* }}} */
+} /* }}} */
 
 /** subArrayFind {{{
  * Find array id of element
- * @param[in] a Subarray
+ * @param[in] a A #SubArray
  * @param[in] elem Element
  * @return Success: Found id
  * 				 Failure: -1
@@ -84,15 +81,14 @@ subArrayFind(SubArray *a,
 
 	assert(a && elem);
 
-	for(i = 0; i < a->size; i++)
+	for(i = 0; i < a->ndata; i++)
 		if(a->data[i] == elem) return(i);
 	return(-1);
-}
-/* }}} */
+} /* }}} */
 
  /** subArraySwap {{{
 	* Swap elements a and b
-	* @param[in] arr Array
+	* @param[in] arr A #SubArray
 	* @param[in] elem1 Element 1
 	* @param[in] elem2 Element 2
 	**/
@@ -104,15 +100,14 @@ subArraySwap(SubArray *a,
 {
 	int i;
 
-	assert(a && a->size >= 1);
+	assert(a && a->ndata >= 1);
 
-	for(i = 0; i < a->size; i++) if(a->data[i] == elem1) a->data[i] = elem2;
-}
-/* }}} */
+	for(i = 0; i < a->ndata; i++) if(a->data[i] == elem1) a->data[i] = elem2;
+} /* }}} */
 
  /** subArrayKill {{{
 	* Kill array and free it's data
-	* @param[in] a
+	* @param[in] a A #SubArray
 	**/
 
 void
@@ -122,8 +117,7 @@ subArrayKill(SubArray *a)
 
 	assert(a);
 
-	for(i = 0; i < a->size; i++)
+	for(i = 0; i < a->ndata; i++)
 		free(a->data[i]);
 	free(a);
-}
-/* }}} */
+} /* }}} */
