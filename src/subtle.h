9c3a47c18dc547b6b12bfaacdc094a8349e316ec
unexist <none@none>
2008-03-21 02:01:15 +0100

    Added/removed function prototypes
    --HG--
    branch : trunk

--- src/subtle.h
+++ src/subtle.h
@@ -1,11 +1,11 @@
 
  /**
 	* subtle - window manager
-	* Copyright (c) 2005-2007 Christoph Kappel
+	* Copyright (c) 2005-2008 Christoph Kappel
 	*
 	* See the COPYING file for the license in the latest tarball.
 	*
-	* $Header$
+	* $Id$
 	**/
 
 #ifndef SUBTLE_H
@@ -45,130 +45,144 @@
 /* }}} */
 
 /* Flags {{{ */
-#define SUB_TYPE_CLIENT				(1L << 1)									// Client
-#define SUB_TYPE_TILE					(1L << 2)									// Tile
+#define SUB_TYPE_CLIENT					(1L << 1)								// Client
+#define SUB_TYPE_TILE						(1L << 2)								// Tile
+#define SUB_TYPE_VERT						(1L << 3)								// Vert tile
+#define SUB_TYPE_HORZ						(1L << 4)								// Horiz tile
+#define SUB_TYPE_VIEW						(1L << 5)								// View
+#define SUB_TYPE_SUBLET					(1L << 6)								// Sublet
+#define SUB_TYPE_KEY						(1L << 7)								// Key
+#define SUB_TYPE_RULE						(1L << 8)								// Rule
 
-#define SUB_TYPE_VERT					(1L << 3)									// Vert tile
-#define SUB_TYPE_HORZ					(1L << 4)									// Horiz tile
+#define SUB_STATE_SHADE					(1L << 9)								// Shaded window
+#define SUB_STATE_FLOAT					(1L << 10)							// Floated window
+#define SUB_STATE_FULL					(1L << 11)							// Fullscreen window
+#define SUB_STATE_RESIZE				(1L << 12)							// Resized window
+#define SUB_STATE_STACK					(1L << 13)							// Stacked tiling window
+#define SUB_STATE_TRANS					(1L << 14)							// Transient window
+#define SUB_STATE_DEAD					(1L << 15)							// Dead window
 
-#define SUB_STATE_SHADE				(1L << 5)									// Shaded window
-#define SUB_STATE_FLOAT				(1L << 6)									// Floated window
-#define SUB_STATE_FULL				(1L << 7)									// Fullscreen window
-#define SUB_STATE_RESIZE			(1L << 8)									// Resized window
-#define SUB_STATE_STACK				(1L << 9)									// Stacked tiling window
-#define SUB_STATE_TRANS				(1L << 10)								// Transient window
-#define SUB_STATE_DEAD				(1L << 11)								// Dead window
+#define SUB_PREF_INPUT					(1L << 16)							// Active/passive focus-model
+#define SUB_PREF_FOCUS					(1L << 17)							// Send focus message
+#define SUB_PREF_CLOSE					(1L << 18)							// Send close message
 
-#define SUB_PREF_INPUT				(1L << 12)								// Active/passive focus-model
-#define SUB_PREF_FOCUS				(1L << 13)								// Send focus message
-#define SUB_PREF_CLOSE				(1L << 14)								// Send close message
+#define SUB_DRAG_START					(1L << 1)								// Drag start
+#define SUB_DRAG_ABOVE					(1L << 2)								// Drag above
+#define SUB_DRAG_BELOW					(1L << 3)								// Drag below
+#define SUB_DRAG_BEFORE					(1L << 4)								// Drag before
+#define SUB_DRAG_AFTER					(1L << 5)								// Drag after
+#define SUB_DRAG_MOVE						(1L << 6)								// Drag move
+#define SUB_DRAG_SWAP						(1L << 7)								// Drag swap
+#define SUB_DRAG_TOP						(1L << 8)								// Drag split horiz top
+#define SUB_DRAG_BOTTOM					(1L << 9)								// Drag split horiz bottom
+#define SUB_DRAG_LEFT						(1L << 10)							// Drag split vert left
+#define SUB_DRAG_RIGHT					(1L << 11)							// Drag split vert right
 
-#define SUB_DRAG_START				(1L << 1)									// Drag start
-#define SUB_DRAG_ABOVE				(1L << 2)									// Drag above
-#define SUB_DRAG_BELOW				(1L << 3)									// Drag below
-#define SUB_DRAG_BEFORE				(1L << 4)									// Drag before
-#define SUB_DRAG_AFTER				(1L << 5)									// Drag after
-#define SUB_DRAG_MOVE					(1L << 6)									// Drag move
-#define SUB_DRAG_SWAP					(1L << 7)									// Drag swap
-#define SUB_DRAG_TOP					(1L << 8)									// Drag split horiz top
-#define SUB_DRAG_BOTTOM				(1L << 9)									// Drag split horiz bottom
-#define SUB_DRAG_LEFT					(1L << 10)								// Drag split vert left
-#define SUB_DRAG_RIGHT				(1L << 11)								// Drag split vert right
+#define SUB_KEY_FOCUS_ABOVE			(1L << 9)								// Focus above window
+#define SUB_KEY_FOCUS_BELOW			(1L << 10)							// Focus below window
+#define SUB_KEY_FOCUS_NEXT			(1L << 11)							// Focus next window
+#define SUB_KEY_FOCUS_PREV			(1L << 12)							// Focus prev window
+#define SUB_KEY_FOCUS_ANY				(1L << 13)							// Focus any window
+#define SUB_KEY_DELETE_WIN			(1L << 14)							// Delete win
+#define SUB_KEY_TOGGLE_SHADE		(1L << 15)							// Toggle collapse
+#define SUB_KEY_TOGGLE_RAISE		(1L << 16)							// Toggle raise
+#define SUB_KEY_TOGGLE_FULL			(1L << 17)							// Toggle fullscreen
+#define SUB_KEY_TOGGLE_PILE			(1L << 18)							// Toggle pile
+#define SUB_KEY_TOGGLE_LAYOUT		(1L << 19)							// Toggle tile layout
+#define SUB_KEY_DESKTOP_NEXT		(1L << 20)							// Switch to next desktop
+#define SUB_KEY_DESKTOP_PREV		(1L << 21)							// Switch to previous desktop
+#define SUB_KEY_DESKTOP_MOVE		(1L << 22)							// Move window to desktop
+#define SUB_KEY_EXEC						(1L << 23)							// Exec an app
 
-#define SUB_KEY_FOCUS_ABOVE		(1L << 1)									// Focus above window
-#define SUB_KEY_FOCUS_BELOW		(1L << 2)									// Focus below window
-#define SUB_KEY_FOCUS_NEXT		(1L << 3)									// Focus next window
-#define SUB_KEY_FOCUS_PREV		(1L << 4)									// Focus prev window
-#define SUB_KEY_FOCUS_ANY			(1L << 5)									// Focus any window
+#define SUB_SUBLET_TYPE_TEXT		(1L << 9)								// Text sublet
+#define SUB_SUBLET_TYPE_TEASER	(1L << 10)							// Teaser sublet
+#define SUB_SUBLET_TYPE_METER		(1L << 11)							// Meter sublet
+#define SUB_SUBLET_TYPE_WATCH		(1L << 12)							// Watch sublet
+#define SUB_SUBLET_TYPE_HELPER	(1L << 13)							// Helper sublet
 
-#define SUB_KEY_DELETE_WIN		(1L << 6)									// Delete win
-#define SUB_KEY_TOGGLE_SHADE	(1L << 7)									// Toggle collapse
-#define SUB_KEY_TOGGLE_RAISE	(1L << 8)									// Toggle raise
-#define SUB_KEY_TOGGLE_FULL		(1L << 9)									// Toggle fullscreen
-#define SUB_KEY_TOGGLE_PILE		(1L << 10)								// Toggle pile
-
-#define SUB_KEY_TOGGLE_LAYOUT	(1L << 11)								// Toggle tile layout
-#define SUB_KEY_DESKTOP_NEXT	(1L << 12)								// Switch to next desktop
-#define SUB_KEY_DESKTOP_PREV	(1L << 13)								// Switch to previous desktop
-#define SUB_KEY_DESKTOP_MOVE	(1L << 14)								// Move window to desktop
-#define SUB_KEY_EXEC					(1L << 15)								// Exec an app
+#define SUB_SUBLET_FAIL_FIRST		(1L << 14)							// Fail first time
+#define SUB_SUBLET_FAIL_SECOND	(1L << 15)							// Fail second time
+#define SUB_SUBLET_FAIL_THIRD		(1L << 16)							// Fail third time
 /* }}} */
 
 /* array.c {{{ */
 typedef struct subarray
 {
+	int		ndata;																					// Array data count
 	void	**data;																					// Array data
-	int		ndata;																					// Array data number
 } SubArray;
+
+typedef struct subarrayundef
+{
+	int flags;																						// Undef flags
+} SubArrayUndef;
 
 SubArray *subArrayNew(void);														// Create new array
 void subArrayPush(SubArray *a, void *elem);							// Push element to array
 void subArrayPop(SubArray *a, void *elem);							// Pop element from array
-void subArraySwap(SubArray *a, void *elem1, 
-	void *elem2);																					// Swap element a and b
-void subArrayKill(SubArray *a);													// Kill array
+int subArrayFind(SubArray *a, void *elem);							// Find array id of element
+void subArrayKill(SubArray *a);													// Kill array with all elements
 /* }}} */
 
 /* client.c {{{ */
 typedef struct subclient
 {
-	int							x, y, width, height, flags;						// Common properties and flags
-	struct subtile 	*tile;																// Parent tile
-	int							n, size;															// ?
+	int							flags, x, y, width, height, size;			// Client flags and common properties
+	struct subtile 	*tile;																// Client parent
 	char						*name;																// Client name
-	Colormap				cmap;																	// Colormap	
-	Window					frame, caption, title, win;						// Decoration windows
-	Window					left, right, bottom;									// Border windows
+	Colormap				cmap;																	// Client colormap	
+	Window					frame, caption, title, win;						// Client decoration windows
+	Window					left, right, bottom;									// Client border windows
 } SubClient;
 
 SubClient *subClientNew(Window win);										// Create new client
-void subClientDelete(SubClient *c);											// Delete client
 void subClientRender(SubClient *c);											// Render client
 void subClientFocus(SubClient *c);											// Focus client
 void subClientResize(SubClient *c);											// Resize client
-void subClientMap(SubClient *c);	
-void subClientUnmap(SubClient *c);
+void subClientMap(SubClient *c);												// Map client	
+void subClientUnmap(SubClient *c);											// Unmap client
 void subClientConfigure(SubClient *c);									// Send configure request
 void subClientDrag(SubClient *c, int mode);							// Move/drag client
 void subClientToggle(SubClient *c, int type);						// Toggle client state
 void subClientFetchName(SubClient *c);									// Fetch client name
 void subClientSetWMState(SubClient *c, long state);			// Set client WM state
 long subClientGetWMState(SubClient *c);									// Get client WM state
+void subClientKill(SubClient *c);												// Kill client
 /* }}} */
 
 /* tile.c {{{ */
 typedef struct subtile
 {
-	int								x, y, width, height, flags;					// Common properties and flags
-	struct subtile		*tile;															// Parent tile
-	struct subclient	*pile;															// Pile top
-	struct subarray		*children;													// Children
+	int								flags, x, y, width, height, size;		// Tile flags and common properties
+	struct subtile		*tile;															// Tile parent
+	struct subclient	*top;																// Tile stack top
+	struct subarray		*clients;														// Tile clients
 } SubTile;
 
 SubTile *subTileNew(int mode);													// Create new tile
-void subTilePush(SubTile *t, void *c);									// Push children to tile
-void subTilePop(SubTile *t, void *c);										// Pop children from tile
-void subTileKill(SubTile *t);														// Kill tile
+void subTilePush(SubTile *t, void *c);									// Push clients to tile
+void subTilePop(SubTile *t, void *c);										// Pop clients from tile
 void subTileConfigure(SubTile *t);											// Configure tile
+void subTileKill(SubTile *t);														// Kill tile
 /* }}} */
 
 /* rule.c {{{ */
 typedef struct subrule
 {
-	int							size;																	// Rule size
+	int							flags, size;													// Rule flags, size
 	regex_t					*regex;																// Rule regex
 	struct subtile	*tile;																// Rule tile
 } SubRule;
 
 SubRule *subRuleNew(char *tags, int size);							// Create rule
-void subRuleDelete(SubRule *r);													// Delete rule
+void subRuleKill(SubRule *r);														// Delete rule
 
 /* }}} */
 
 /* view.c {{{ */
 typedef struct subview
 {
-	int							width, xid;														// View button width, view xid
+	int							flags, width;													// View flags and button width
 	Window					frame, button;												// View frame, button
 	char 						*name;																// View name
 	struct subtile	*tile;																// View tile
@@ -183,32 +197,22 @@ void subViewMerge(Window win);													// Merge window
 void subViewRender(void);																// Render views
 void subViewConfigure(void);														// Configure views
 void subViewSwitch(SubView *v);													// Switch view
-void subViewKill(void);																	// Kill views
+void subViewKill(SubView *v);														// Kill views
 
 /* }}} */
 
 /* sublet.c {{{ */
-#define SUB_SUBLET_TYPE_TEXT		(1L << 1)								// Text sublet
-#define SUB_SUBLET_TYPE_TEASER	(1L << 2)								// Teaser sublet
-#define SUB_SUBLET_TYPE_METER		(1L << 3)								// Meter sublet
-#define SUB_SUBLET_TYPE_WATCH		(1L << 4)								// Watch sublet
-#define SUB_SUBLET_TYPE_HELPER	(1L << 5)								// Helper sublet
-
-#define SUB_SUBLET_FAIL_FIRST		(1L << 6)								// Fail first time
-#define SUB_SUBLET_FAIL_SECOND	(1L << 7)								// Fail second time
-#define SUB_SUBLET_FAIL_THIRD		(1L << 8)								// Fail third time
-
 typedef struct subsublet
 {
-	int			flags, ref;																		// Flags, Lua object reference, width
-	time_t	time, interval;																// Last update time, interval time
+	int			flags, ref, width;														// Sublet flags, object reference, width
+	time_t	time, interval;																// Sublet update time, interval time
 
-	struct subsublet *next;																// Next sibling
+	struct subsublet *next;																// Sublet next sibling
 
 	union 
 	{
-		char *string;																				// String data
-		int number;																					// Number data
+		char *string;																				// Sublet data string
+		int number;																					// Sublet data number
 	};
 } SubSublet;
 
@@ -220,7 +224,7 @@ void subSubletRender(void);															// Render sublet
 void subSubletConfigure(void);													// Configure sublet bar
 void subSubletMerge(int pos);														// Merge sublet
 SubSublet *subSubletNext(void);													// Get next sublet
-void subSubletKill(void);																// Delete sublets
+void subSubletKill(SubSublet *s);												// Delete sublets
 /* }}} */
 
 /* display.c {{{ */
@@ -236,6 +240,7 @@ typedef struct subdisplay
 	struct subarray		*views;															// View list
 	struct subarray		*clients;														// Client list
 	struct subarray		*sublets;														// Sublet list
+	struct subarray		*keys;															// Key list
 
 #ifdef HAVE_SYS_INOTIFY_H
 	int								notify;															// Inotify descriptor
@@ -272,13 +277,13 @@ void subDisplayScan(void);															// Scan root window
 /* key.c {{{ */
 typedef struct subkey
 {
-	int flags, code;
-	unsigned int mod;
+	int flags, code;																			// Key flags, code
+	unsigned int mod;																			// Key modifier
 
 	union
 	{
-		char *string;
-		int number;
+		char *string;																				// Key data string
+		int number;																					// Key data number
 	};
 } SubKey;
 
@@ -288,7 +293,7 @@ void subKeyNew(const char *key,
 	const char *value);																		// Create new key
 void subKeyGrab(SubClient *c);													// Grab keys for a window
 void subKeyUngrab(SubClient *c);												// Ungrab keys for a window
-void subKeyKill(void);																	// Delete all keys
+void subKeyKill(SubKey *k);															// Delete all keys
 /* }}} */
 
 /* lua.c {{{ */
@@ -299,7 +304,6 @@ void subLuaCall(SubSublet *s);													// Call Lua script
 /* }}} */
 
 /* event.c {{{ */
-time_t subEventGetTime(void);														// Get the current time
 int subEventLoop(void);																	// Event loop
 /* }}} */
 
@@ -319,6 +323,7 @@ void subUtilLog(int type, const char *file,
 void *subUtilRealloc(void *mem, size_t size);						// Reallocate memory
 void *subUtilAlloc(size_t n, size_t size);							// Allocate memory
 XPointer *subUtilFind(Window win, XContext id);					// Find window data
+time_t subUtilTime(void);																// Get the current time
 /* }}} */
 
 /* ewmh.c  {{{ */
