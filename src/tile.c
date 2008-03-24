baac6232aae579bc668c79652624188f760cf4a7
unexist <none@none>
2008-03-24 03:17:49 +0100

    Removed subTilePush() and subTilePop()
    --HG--
    branch : trunk

--- src/tile.c
+++ src/tile.c
@@ -25,43 +25,16 @@ subTileNew(int mode)
 	assert(mode == SUB_TYPE_HORZ || mode == SUB_TYPE_VERT);
 
 	t	= (SubTile *)subUtilAlloc(1, sizeof(SubTile));
-	t->flags = SUB_TYPE_TILE|mode;
+	t->flags		= SUB_TYPE_TILE|mode;
+	t->clients	= subArrayNew();
 
-	subUtilLogDebug("type=%s\n", (mode & SUB_TYPE_HORZ) ? "horz" : "vert");
+	/* Start values */
+	t->width	= DisplayWidth(d->disp, DefaultScreen(d->disp));
+	t->height	= DisplayHeight(d->disp, DefaultScreen(d->disp)) - d->th;
+
+	subUtilLogDebug("new=tile, type=%s\n", (mode & SUB_TYPE_HORZ) ? "horz" : "vert");
 
 	return(t);
-} /* }}} */
-
- /** subTilePush {{{
-	* Push clients to tile
-	* @param[in] t A #SubTile
-	* @param[in] c A clients
-	**/
-
-void
-subTilePush(SubTile *t,
-	void *c)
-{
-	assert(t && c);
-
-	subArrayPush(t->clients, (void *)c);
-	((SubClient *)c)->tile = t;
-} /* }}} */
-
- /** subTilePop {{{
-	* Pop clients from tile
-	* @param[in] t A #SubTile
-	* @param[in] c A clients
-	**/
-
-void
-subTilePop(SubTile *t,
-	void *c)
-{
-	assert(t && c);
-
-	subArrayPop(t->clients, c);
-	((SubClient *)c)->tile = NULL;
 } /* }}} */
 
  /** subTileConfigure {{{
@@ -75,19 +48,18 @@ subTileConfigure(SubTile *t)
 	int i;
 	SubClient *c = NULL;
 	unsigned int x = 0, y = 0, ch = 0, cw = 0, width = 0, height = 0, size = 0, 
-		comp = 0, shaded = 0, resized = 0, full = 0, floated = 0, nclients = 0;
+		comp = 0, shaded = 0, resized = 0, full = 0, floated = 0, n = 0;
 
 	assert(t);
 
-	width			= t->width;
-	height		= t->height;
-	nclients	= t->clients->ndata;
-
-	if(nclients > 0)
+	if(t->clients->ndata > 0)
 		{
-			size = (t->flags & SUB_TYPE_HORZ) ? width : height;
+			n			= t->clients->ndata;
+			width	= t->width;
+			height= t->height;
+			size	= (t->flags & SUB_TYPE_HORZ) ? width : height;
 
-			/* Find special clients */
+		/* Find special clients */
 			for(i = 0; i < t->clients->ndata; i++)
 				{
 					c = (SubClient *)t->clients->data[i]; /* Both types have common fields */
@@ -98,7 +70,7 @@ subTileConfigure(SubTile *t)
 					else if(c->flags & SUB_STATE_RESIZE && c->size > 0)
 						{
 							/* Prevent resized windows only */
-							if(resized == nclients)
+							if(resized == n)
 								{
 									c->flags &= ~SUB_STATE_RESIZE;
 									c->size = 0;
@@ -111,10 +83,13 @@ subTileConfigure(SubTile *t)
 						}
 				}
 
+			subUtilLogDebug("type=%s, n=%d, sha=%d, ful=%d, flo=%d, res=%d\n", (t->flags & SUB_TYPE_HORZ) ? "horz" : "vert",
+				n, shaded, full, floated, resized);
+
 			/* Stacked window */
 			if(t->flags & SUB_STATE_STACK) 
 				{
-					shaded = nclients - 1;
+					shaded = n - 1;
 					resized	= 0;
 				}
 
@@ -126,26 +101,27 @@ subTileConfigure(SubTile *t)
 				}
 
 			 /* Prevent division by zero */
-			nclients	= (nclients - shaded - resized - full - floated) > 0 ? nclients - shaded - resized - full - floated : 1;
-			cw				= (t->flags & SUB_TYPE_HORZ) ? width / nclients : width;
-			ch				= (t->flags & SUB_TYPE_VERT) ? (height - shaded * d->th) / nclients : height;
+			n		= (n - shaded - resized - full - floated) > 0 ? n - shaded - resized - full - floated : 1;
+			cw	= (t->flags & SUB_TYPE_HORZ) ? width / n : width;
+			ch	= (t->flags & SUB_TYPE_VERT) ? (height - shaded * d->th) / n : height;
+
+			assert(cw > 0 && ch > 0);
 
 			/* Get compensation for int rounding */
-			if(t->flags & SUB_TYPE_HORZ) comp = abs(width - nclients * cw - shaded * d->th);
-			else comp = abs(height - nclients * ch - shaded * d->th);
+			if(t->flags & SUB_TYPE_HORZ) comp = abs(width - n * cw - shaded * d->th);
+			else comp = abs(height - n * ch - shaded * d->th);
 
 			for(i = 0; i < t->clients->ndata; i++)
 				{
 					c = (SubClient *)t->clients->data[i]; /* Both types have common fields */
-					
+
 					if(!(c->flags & (SUB_STATE_TRANS|SUB_STATE_FLOAT|SUB_STATE_FULL)))
 						{
 							/* Shade every client that is not on top */
-							if(t->flags & SUB_STATE_STACK && t->tile->top != c) 
-								c->flags |= SUB_STATE_SHADE;
+							if(t->flags & SUB_STATE_STACK && t->tile->top != c) c->flags |= SUB_STATE_SHADE;
 
 							/* Remove tiles with only one client */
-							if(c->flags & SUB_TYPE_TILE && ((SubTile *)c)->clients->ndata == 1)
+							if(c->flags & SUB_TYPE_TILE && !(c->flags & SUB_TYPE_RULE) && ((SubTile *)c)->clients->ndata == 1)
 								{
 									SubTile *tmp = (SubTile *)t->clients->data[i];
 									t->clients->data[i] = tmp->clients->data[0];
@@ -164,44 +140,45 @@ subTileConfigure(SubTile *t)
 							/* Adjust sizes according to the tile alignment */
 							if(t->flags & SUB_TYPE_HORZ)
 								{
+									if(shaded > 0) c->height = ch - shaded * d->th;
 									if(!(c->flags & SUB_STATE_SHADE)) 
 										{
 											c->x = x;
-											x += c->width; /* Adjust x */
+											if(c->flags & SUB_STATE_RESIZE) c->width = t->width * c->size / 100;
 										}
-									if(c->flags & SUB_STATE_SHADE) 
-										{
-											c->width = SUBWINWIDTH(t);
-											y += d->th; /* Adjust y */
-										}
-									else if(c->flags & SUB_STATE_RESIZE) c->width = t->width * c->size / 100;
-
-									if(shaded > 0) c->height = ch - shaded * d->th;
+									else c->width = t->width;
 								}
 							else if(t->flags & SUB_TYPE_VERT)
 								{
 									c->y = y;
-									y += c->height; /* Adjust y again */
 									if(c->flags & SUB_STATE_RESIZE) c->height = t->height * c->size / 100;
 								}
 
+							c->x += t->x;
+							c->y += t->y;
+
 							/* Add compensation to width or height */
-							if(t->clients->ndata == i) 
+							if(t->clients->ndata == i + 1) 
 								{
 									if(t->flags & SUB_TYPE_HORZ) c->width += comp; 
 									else c->height += comp;
 								}
 
-							if(c->flags & SUB_TYPE_TILE) subTileConfigure((SubTile *)t->clients->data[i]);
-							else if(c->flags & SUB_TYPE_CLIENT) 
+							/* Store size */
+							if(t->flags & SUB_TYPE_HORZ) 
 								{
-									subClientResize(c);
-									subClientConfigure(c);
+									if(c->flags & SUB_STATE_SHADE) y += d->th;
+									else x += c->width;
 								}
+							else y += c->height;
+
+							subUtilLogDebug("[%p] type=%c, x=%03d, y=%03d, w=%03d, h=%03d\n", &c, c->flags & SUB_TYPE_CLIENT ? 'c' : 't', 
+								c->x, c->y, c->width, c->height);
+
+							if(c->flags & SUB_TYPE_TILE) subTileConfigure((SubTile *)t->clients->data[i]);
+							else if(c->flags & SUB_TYPE_CLIENT) subClientConfigure(c);
 						}
 				}
-			subUtilLogDebug("type=%s, nclients=%d, cw=%d, ch=%d\n", 
-				(t->flags & SUB_TYPE_HORZ) ? "horz" : "vert", nclients, cw, ch);
 		}
 } /* }}} */
 
@@ -212,14 +189,8 @@ subTileConfigure(SubTile *t)
 void
 subTileKill(SubTile *t)
 {
-	int i;
-
-	for(i = 0; i < t->clients->ndata; i++)
-		{
-			if(((SubClient *)t->clients->data[i])->flags & SUB_TYPE_CLIENT) subClientKill((SubClient *)t->clients->data[i]);
-			else if(((SubClient *)t->clients->data[i])->flags & SUB_TYPE_TILE) subTileKill((SubTile *)t->clients->data[i]);
-		}
-	
-	subArrayKill(t->clients);
+	subArrayKill(t->clients, True);
 	free(t);
+
+	subUtilLogDebug("kill=tile\n");
 } /* }}} */
