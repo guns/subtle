26d5fd4bdeab236304191e9eaaf2950dca585972
unexist <none@none>
2008-10-05 21:34:50 +0200

    Added view and tag class
    --HG--
    branch : trunk

--- src/subtlext/subtlext.c
+++ src/subtlext/subtlext.c
@@ -16,8 +16,8 @@
 Display *display = NULL;
 int debug = 0;
 
-static VALUE subtle = Qnil;
-static int refs = 0;
+static VALUE subtle = Qnil, view = Qnil, tag = Qnil, name = Qnil; ///< Common used symbols
+static int refcount = 0;
 
 static void SubtleKill(void *data);
 
@@ -44,7 +44,7 @@ SubtleNew(int argc,
       XSetErrorHandler(subSharedLogXError);
       subSharedLogDebug("Connection opened (%s)\n", dispname);
     }
-  refs++;
+  refcount++;
 
   /* Create instance */
   data = Data_Wrap_Struct(self, 0, SubtleKill, display);
@@ -53,28 +53,30 @@ SubtleNew(int argc,
   return(data);
 } /* }}} */
 
+/* SubtleRunning {{{ */
 static VALUE
 SubtleRunning(VALUE self)
 {
   char *prop = NULL;
+  VALUE ret = Qfalse;
   
   prop = subSharedPropertyGet(DefaultRootWindow(display), XInternAtom(display, "UTF8_STRING", False),
     "_NET_WM_NAME", NULL);
   if(prop) 
     {
-      printf("prop=%s\n", prop);
+      if(!strncmp(prop, PKG_NAME, strlen(prop))) ret = Qtrue;
+      subSharedLogDebug("wmname=%s\n", prop);
       free(prop);
-      return(Qtrue);
     }
 
-  return(Qfalse);
-}
+  return(ret);
+} /* }}} */
 
 /* SubtleKill {{{ */
 static void
 SubtleKill(void *data)
 {
-  if(0 == --refs) 
+  if(0 == --refcount) 
     {
       XCloseDisplay(display);
       display = NULL;
@@ -96,14 +98,129 @@ SubtleDisplay(VALUE self)
   return(rb_str_new2(DisplayString(display)));
 } /* }}} */
 
+/* SubtleViews {{{ */
+static VALUE
+SubtleViews(VALUE self)
+{
+  int i, size = 0;
+  char **views = NULL;
+  VALUE array = Qnil, method = Qnil, klass = Qnil;
+  
+  method = rb_intern("new");
+  klass  = rb_const_get(rb_mKernel, rb_intern("View"));
+  views  = subSharedPropertyList(DefaultRootWindow(display), "_NET_DESKTOP_NAMES", &size);
+  array  = rb_ary_new2(size);
+
+  for(i = 0; i < size; i++)
+    {
+      VALUE v = rb_funcall(klass, method, 1, rb_str_new2(views[i]));
+      rb_ary_push(array, v);
+    }
+
+  return(array);
+} /* }}} */
+
+/* SubtleTags {{{ */
+static VALUE
+SubtleTags(VALUE self)
+{
+  int i, size = 0;
+  char **tags = NULL;
+  VALUE array = Qnil, method = Qnil, klass = Qnil;
+  
+  method = rb_intern("new");
+  klass  = rb_const_get(rb_mKernel, rb_intern("View"));
+  tags   = subSharedPropertyList(DefaultRootWindow(display), "SUBTLE_TAG_LIST", &size);
+  array  = rb_ary_new2(size);
+
+  for(i = 0; i < size; i++)
+    {
+      VALUE t = rb_funcall(klass, method, 1, rb_str_new2(tags[i]));
+      rb_ary_push(array, t);
+    }
+
+  return(array);
+} /* }}} */
+
+
+/* ViewInit {{{ */
+static VALUE
+ViewInit(VALUE self,
+  VALUE vname)
+{
+  rb_ivar_set(self, name, vname);
+
+  return(self);
+} /* }}} */
+
+/* ViewToString {{{ */
+static VALUE
+ViewToString(VALUE self)
+{
+  VALUE result = rb_ivar_get(self, name);
+
+  if(T_STRING != rb_type(result))
+    {
+      subSharedLogWarn("Unknown value type\n");
+      return(Qnil);
+    }
+
+  return(result);
+} /* }}} */
+
+/* TagInit {{{ */
+static VALUE
+TagInit(VALUE self,
+  VALUE vname)
+{
+  rb_ivar_set(self, name, vname);
+
+  return(self);
+} /* }}} */
+
+/* TagToString {{{ */
+static VALUE
+TagToString(VALUE self)
+{
+  VALUE result = rb_ivar_get(self, name);
+
+  if(T_STRING != rb_type(result))
+    {
+      subSharedLogWarn("Unknown value type\n");
+      return(Qnil);
+    }
+
+  return(result);
+} /* }}} */
+
 void 
 Init_subtlext(void)
 {
+  /* Class: subtle */
 	subtle = rb_define_class("Subtle", rb_cObject);
   rb_define_singleton_method(subtle, "new", SubtleNew, -1);
 	rb_define_method(subtle, "version", SubtleVersion, 0);
-
 	rb_define_method(subtle, "display", SubtleDisplay, 0);
+	rb_define_method(subtle, "views", SubtleViews, 0);
+	rb_define_method(subtle, "tags", SubtleTags, 0);
 	rb_define_method(subtle, "running?", SubtleRunning, 0);
 
+  /* Class: view */
+  view = rb_define_class("View", rb_cObject);
+  rb_define_method(view, "initialize", ViewInit, 1);
+  rb_define_method(view, "to_s", ViewToString, 0);
+
+  /* Class: tag */
+  tag = rb_define_class("Tag", rb_cObject);
+  rb_define_method(tag, "initialize", TagInit, 1);
+  rb_define_method(tag, "to_s", TagToString, 0);
+
+  /* Attrs: view */
+  rb_define_attr(view, "name", 1, 1);
+
+  /* Attrs: tag */
+  rb_define_attr(tag, "name", 1, 1);
+
+  /* Common used symbols */
+  name = rb_intern("@name");
 }
