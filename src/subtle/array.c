8cfe2b8e1c36ebbf0bd18f7f6180560cdcbacc2a
unexist <none@none>
2008-12-04 20:57:42 +0100

    Added kill handlers for subtray_t
    --HG--
    branch : trunk

--- src/subtle/array.c
+++ src/subtle/array.c
@@ -139,15 +139,17 @@ subArrayKill(SubArray *a,
       /* Check type and kill */
       SubClient *c = CLIENT(a->data[i]);
 
+      /* Common types first */
       if(c->flags & SUB_TYPE_CLIENT)      subClientKill(CLIENT(c));
-      else if(c->flags & SUB_TYPE_GRAB)   subGrabKill(GRAB(c));
-      else if(c->flags & SUB_TYPE_LAYOUT) subLayoutKill(LAYOUT(c));
-      else if(c->flags & SUB_TYPE_SUBLET) subSubletKill(SUBLET(c));
       else if(c->flags & SUB_TYPE_TAG)    subTagKill(TAG(c));
       else if(c->flags & SUB_TYPE_VIEW)   subViewKill(VIEW(c));
+      else if(c->flags & SUB_TYPE_LAYOUT) subLayoutKill(LAYOUT(c));
+      else if(c->flags & SUB_TYPE_GRAB)   subGrabKill(GRAB(c));
+      else if(c->flags & SUB_TYPE_SUBLET) subSubletKill(SUBLET(c));
+      else if(c->flags & SUB_TYPE_TRAY)   subTrayKill(TRAY(c));
       else free(a->data[i]); 
     }
 
   if(a->data) free(a->data);
   free(a);
-} /* }}} */
+} /* }}} */#define LENGTH(a) (sizeof(a) / sizeof(a[0])) 
