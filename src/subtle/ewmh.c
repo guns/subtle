5facd34e6daeb449774afd5c968c731644a232c4
unexist <none@none>
2008-12-07 13:56:33 +0100

    Added subEwmhMessage()
    --HG--
    branch : trunk

--- src/subtle/ewmh.c
+++ src/subtle/ewmh.c
@@ -13,8 +13,7 @@
 #include <X11/Xatom.h>
 #include "subtle.h"
 
-#define NATOMS 52
-static Atom atoms[NATOMS];
+static Atom atoms[SUB_EWMH_TOTAL];
 
  /** subEwmhInit {{{
   * @brief Init and register ICCCM/EWMH atoms
@@ -23,7 +22,8 @@ static Atom atoms[NATOMS];
 void
 subEwmhInit(void)
 {
-  char *names[] =
+  int len = 0;
+  char *selection = NULL, *names[] =
   {
     /* ICCCM */
     "WM_NAME", "WM_CLASS", "WM_STATE", "WM_PROTOCOLS", "WM_TAKE_FOCUS", "WM_DELETE_WINDOW", "WM_NORMAL_HINTS", "WM_SIZE_HINTS",
@@ -37,9 +37,13 @@ subEwmhInit(void)
     "_NET_WM_WINDOW_TYPE", "_NET_WM_WINDOW_TYPE_DESKTOP", "_NET_WM_WINDOW_TYPE_NORMAL", "_NET_WM_WINDOW_TYPE_DIALOG",
     "_NET_WM_ALLOWED_ACTIONS", "_NET_WM_ACTION_MOVE", "_NET_WM_ACTION_RESIZE",
     "_NET_WM_ACTION_FULLSCREEN", "_NET_WM_ACTION_CHANGE_DESKTOP", "_NET_WM_ACTION_CLOSE",
+    "_NET_SYSTEM_TRAY_OPCODE", "_NET_SYSTEM_TRAY_MESSAGE_DATA", "_NET_SYSTEM_TRAY_S",
 
     /* Misc */
-    "UTF8_STRING",
+    "UTF8_STRING", "MANAGER",
+
+    /* XEmbed */
+    "_XEMBED", "_XEMBED_INFO",
 
     /* subtle */
     "SUBTLE_CLIENT_TAG", "SUBTLE_CLIENT_UNTAG", "SUBTLE_CLIENT_TAGS",
@@ -47,28 +51,58 @@ subEwmhInit(void)
     "SUBTLE_VIEW_NEW", "SUBTLE_VIEW_KILL", "SUBTLE_VIEW_LIST", "SUBTLE_VIEW_TAG", "SUBTLE_VIEW_UNTAG", "SUBTLE_VIEW_TAGS"
   };
 
-  XInternAtoms(subtle->disp, names, NATOMS, 0, atoms);
+  /* Tray selection */
+  len       = strlen(names[SUB_EWMH_NET_SYSTEM_TRAY_SELECTION]) + 5; ///< @todo For high screen counts
+  selection = (char *)subUtilAlloc(len, sizeof(char)); 
+
+  snprintf(selection, len, "%s%u", names[SUB_EWMH_NET_SYSTEM_TRAY_SELECTION], SCREEN);
+  printf("Selection: len=%d, name=%s\n", len, selection);
+  names[SUB_EWMH_NET_SYSTEM_TRAY_SELECTION] = selection;
+
+  XInternAtoms(subtle->disp, names, SUB_EWMH_TOTAL, 0, atoms);
+
+  free(selection);
 } /* }}} */
 
- /** subEwmhFind {{{
+ /** subEwmhGet {{{
   * @brief Find intern atoms
-  * @param[in]  hint  Hint number
-  * @return Returns a #Atom
+  * @param[in]  e  A #SubEwmh
+  * @return Returns the desired #Atom
   **/
 
 Atom
-subEwmhFind(int hint)
+subEwmhGet(SubEwmh e)
 {
-  assert(hint <= NATOMS);
+  assert(e <= SUB_EWMH_TOTAL);
 
-  return atoms[hint];
+  return atoms[e];
+} /* }}} */
+
+ /** subEwmhFind {{{
+  * @brief Find id for intern atom
+  * @param[in]  e  #SubEwmh
+  * @retval  >=0  Found index
+  * @retval  -1   Atom was not found
+  **/
+
+SubEwmh
+subEwmhFind(Atom atom)
+{
+  int i;
+
+  assert(atom);
+
+  for(i = 0; i < SUB_EWMH_TOTAL; i++)
+    if(atoms[i] == atom) return i;
+
+  return -1;
 } /* }}} */
 
  /** subEwmhGetProperty {{{
   * @brief Get property from window
   * @param[in]   win   Window
   * @param[in]   type  Atom type
-  * @param[in]   hint  Hint number
+  * @param[in]   e     A #SubEwmh
   * @param[out]  size  Size of items
   * @return Returns property data
   **/
@@ -76,7 +110,7 @@ subEwmhFind(int hint)
 char *
 subEwmhGetProperty(Window win,
   Atom type,
-  int hint,
+  SubEwmh e,
   unsigned long *size)
 {
   unsigned long nitems, bytes;
@@ -84,15 +118,15 @@ subEwmhGetProperty(Window win,
   int format;
   Atom rtype;
 
-  if(XGetWindowProperty(subtle->disp, win, atoms[hint], 0L, 1024, False, type, &rtype,
+  if(XGetWindowProperty(subtle->disp, win, atoms[e], 0L, 1024, False, type, &rtype,
     &format, &nitems, &bytes, &data) != Success)
     {
-      subUtilLogDebug("Failed to get property (%d)\n", hint);
+      subUtilLogDebug("Failed to get property (%d)\n", e);
       return NULL;
     }
   if(type != rtype)
     {
-      subUtilLogDebug("Invalid type for property (%d)\n", hint);
+      subUtilLogDebug("Invalid type for property (%d <=> %d)\n", type, rtype);
       XFree(data);
       return NULL;
     }
@@ -104,66 +138,66 @@ subEwmhGetProperty(Window win,
  /** subEwmhSetWindows {{{
   * @brief Change window property
   * @param[in]  win     Window
-  * @param[in]  hint    Hint number
+  * @param[in]  e       A #SubEwmh
   * @param[in]  values  Window list
   * @param[in]  size    Size of the list
   **/
 
 void
 subEwmhSetWindows(Window win,
-  int hint,
+  SubEwmh e,
   Window *values,
   int size)
 {
-  XChangeProperty(subtle->disp, win, atoms[hint], XA_WINDOW, 32, PropModeReplace,
+  XChangeProperty(subtle->disp, win, atoms[e], XA_WINDOW, 32, PropModeReplace,
     (unsigned char *)values, size);
 } /* }}} */
 
  /** subEwmhSetCardinals {{{
   * @brief Change window property
   * @param[in]  win     Window
-  * @param[in]  hint    Hint number
+  * @param[in]  e       A #SubEwmh
   * @param[in]  values  Cardinal list
   * @param[in]  size    Size of the list
   **/
 
 void
 subEwmhSetCardinals(Window win,
-  int hint,
+  SubEwmh e,
   long *values,
   int size)
 {
-  XChangeProperty(subtle->disp, win, atoms[hint], XA_CARDINAL, 32, PropModeReplace,
+  XChangeProperty(subtle->disp, win, atoms[e], XA_CARDINAL, 32, PropModeReplace,
     (unsigned char *)values, size);
 } /* }}} */
 
  /** subEwmhSetString {{{
   * @brief Change window property
   * @param[in]  win    Window
-  * @param[in]  hint   Hint number
+  * @param[in]  e      A #SubEwmh
   * @param[in]  value  String value
   **/
 
 void
 subEwmhSetString(Window win,
-  int hint,
+  SubEwmh e,
   char *value)
 {
-  XChangeProperty(subtle->disp, win, atoms[hint], atoms[SUB_EWMH_UTF8], 8,
+  XChangeProperty(subtle->disp, win, atoms[e], atoms[SUB_EWMH_UTF8], 8,
     PropModeReplace, (unsigned char *)value, strlen(value));
 } /* }}} */
 
  /** subEwmhSetStrings {{{
   * @brief Change window property
   * @param[in]  win     Window
-  * @param[in]  hint    Hint number
+  * @param[in]  e       A #SubEwmh
   * @param[in]  values  String list
   * @param[in]  size    Size of the list
   **/
 
 void
 subEwmhSetStrings(Window win,
-  int hint,
+  SubEwmh e,
   char **values,
   int size)
 {
@@ -182,7 +216,49 @@ subEwmhSetStrings(Window win,
       pos++;
     }
 
-  XChangeProperty(subtle->disp, win, atoms[hint], atoms[SUB_EWMH_UTF8], 8,
+  XChangeProperty(subtle->disp, win, atoms[e], atoms[SUB_EWMH_UTF8], 8,
     PropModeReplace, (unsigned char *)str, pos);
   free(str);
 } /* }}} */
+
+ /** subEwmhMessage {{{
+  * @brief Send message
+  * @param[in]  dst    Destination window
+  * @param[in]  win    Target window
+  * @param[in]  e      A #SubEwmh
+  * @param[in]  data0  Data part 1
+  * @param[in]  data1  Data part 2
+  * @param[in]  data2  Data part 3
+  * @param[in]  data3  Data part 4
+  * @param[in]  data4  Data part 5
+  * @retval  0   Error
+  * @retval  >0  Success
+  **/
+
+int
+subEwmhMessage(Window dst,
+  Window win,
+  SubEwmh e,
+  long data0,
+  long data1,
+  long data2,
+  long data3,
+  long data4)
+{
+  XClientMessageEvent ev;
+
+  /* Assemble message */
+  ev.type         = ClientMessage;
+  ev.message_type = atoms[e];
+  ev.window       = win;
+  ev.format       = 32;
+  ev.data.l[0]    = data0;
+  ev.data.l[1]    = data1;
+  ev.data.l[2]    = data2;
+  ev.data.l[3]    = data3;
+  ev.data.l[4]    = data4;
+
+  return XSendEvent(subtle->disp, dst, False, NoEventMask, (XEvent *)&ev);
+} /* }}} */
+
+// vim:ts=2:bs=2:sw=2:et:fdm=marker
