97ef2e13ed7a8acc65fe30246461e210a3ba27f7
unexist <none@none>
2009-03-25 22:17:58 +0100

    Tidied up gravity code and fixed integer division bug
    --HG--
    branch : trunk

--- src/subtle/gravity.c
+++ src/subtle/gravity.c
@@ -13,74 +13,14 @@
 #include "subtle.h"
 
 /* Typedef {{{ */
-typedef struct gravity_t
+typedef struct gravityprops_t
 {
   int grav_right;
   int grav_down;
   int cells_x;
   int cells_y;
 } GravityProps;
-
-static const Gravity gridprops[] =
-{
-  {0,1, 1,1},
-
-  {0,1, 2,2},
-  {0,1, 1,2},
-  {1,1, 2,2},
-
-  {0,0, 2,1},
-  {0,0, 1,1},
-  {1,0, 2,1},
-
-  {0,0, 2,2},
-  {0,0, 1,2},
-  {1,0, 2,2},
-}; /* }}} */
-
-/* GravitySlotToRect {{{ */
-static void
-GravitySlotToRect(XRectangle *r,
-  XRectangle *slot,
-  XRectangle *rect)
-{
-  rect->x      = slot->x;      // slot->x + w->input.left
-  rect->y      = slot->y;      // slot->y + w->input.top;
-  rect->width  = slot->width;  // slot->width - (w->input.left + w->input.right);
-  rect->height = slot->height; // slot->height - (w->input.top + w->input.bottom);
-} /* }}} */
-
-/* GravityConstrainSize {{{ */
-static void
-GravityConstrainSize(XRectangle *r,
-  XRectangle *slot,
-  XRectangle *rect)
-{
-  //int cw, ch;
-  //Rect workarea = { 0, 0, WIDTH, HEIGHT };
-  XRectangle sr;
-
-  GravitySlotToRect(r, slot, &sr);
-
-#if 0
-  if(constrainNewWindowSize(w, r.width, r.height, &cw, &ch))
-    {
-      /* constrained size may put window offscreen, adjust for that case */
-      int dx = r.x + cw - workarea.width - workarea.x + w->input.right;
-      int dy = r.y + ch - workarea.height - workarea.y + w->input.bottom;
-
-      if(dx > 0)
-        r.x -= dx;
-      if(dy > 0)
-        r.y -= dy;
-
-      r.width = cw;
-      r.height = ch;
-  }
-#endif
-
-  *rect = sr;
-} /* }}} */
+/* }}} */
 
   /** subGravityCalc {{{ 
    * @brief Calc rect in grid
@@ -93,75 +33,79 @@ subGravityCalc(XRectangle *r,
   int type)
 {
   XRectangle workarea = { 0, 0, SCREENW, SCREENH - subtle->th };
-  XRectangle desiredSlot = { 0 }, desiredRect = { 0 }, currentRect = { 0 };
-  GravityProps props = gridprops[type];
+  XRectangle slot = { 0 }, desired = { 0 }, current = { 0 };
+
+  static const GravityProps props[] =
+  {
+    { 0, 1, 1, 1 }, ///< Gravity unknown
+    { 0, 1, 2, 2 }, ///< Gravity bottom left
+    { 0, 1, 1, 2 }, ///< Gravity bottom
+    { 1, 1, 2, 2 }, ///< Gravity bottom right
+    { 0, 0, 2, 1 }, ///< Gravity left
+    { 0, 0, 1, 1 }, ///< Gravity center
+    { 1, 0, 2, 1 }, ///< Gravity right
+    { 0, 0, 2, 2 }, ///< Gravity top left
+    { 0, 0, 1, 2 }, ///< Gravity top
+    { 1, 0, 2, 2 }, ///< Gravity top right
+  }; 
 
   assert(r);
-printf("DEBUG\n");
-  /* slice and dice to get desired slot - including decorations */
-  desiredSlot.y      = workarea.y + props.grav_down * (workarea.height / props.cells_y);
-  desiredSlot.height = workarea.height / props.cells_y;
-  desiredSlot.x      = workarea.x + props.grav_right * (workarea.width / props.cells_x);
-  desiredSlot.width  = workarea.width / props.cells_x;
 
-  /* Adjust for constraints and decorations */
-  GravityConstrainSize(r, &desiredSlot, &desiredRect);
+  /* Compute slot */
+  slot.x      = workarea.x + props[type].grav_right * (workarea.width / props[type].cells_x);
+  slot.y      = workarea.y + props[type].grav_down * (workarea.height / props[type].cells_y);
+  slot.height = workarea.height / props[type].cells_y;
+  slot.width  = workarea.width / props[type].cells_x;
 
-  /* Get current rect not including decorations */
-  currentRect.x      = r->x; 
-  currentRect.y      = r->y;
-  currentRect.width  = r->width;
-  currentRect.height = r->height;
+  desired = slot;
+  current = *r;
 
-	if(desiredRect.y == currentRect.y && desiredRect.height == currentRect.height)
+	if(desired.y == current.y && desired.height == current.height)
 	  {
-	    int slotWidth33  = workarea.width / 3;
-	    int slotWidth66  = workarea.width - slotWidth33;
+	    int width33 = workarea.width / 3;
+	    int width66 = workarea.width - width33;
+      int comp    = abs(workarea.width - 3 * width33); ///< Int rounding
 
-      printf("Multi!\n");
-
-	    if(props.cells_x == 2)
+	    if(props[type].cells_x == 2)
 	      {
-	       if(currentRect.width == desiredRect.width && currentRect.x == desiredRect.x)
+	       if(current.width == desired.width && current.x == desired.x)
 	         {
-	           desiredSlot.width = slotWidth66;
-	           desiredSlot.x = workarea.x + props.grav_right * slotWidth33;
+	           slot.x     = workarea.x + props[type].grav_right * width33;
+	           slot.width = width66;
         		}
       		else
         		{
-              XRectangle rect33, rect66, slot33, slot66;
+              XRectangle rect33, rect66;
 
-              slot33       = desiredSlot;
-              slot33.x     = workarea.x + props.grav_right * slotWidth66;
-              slot33.width = slotWidth33;
-              GravityConstrainSize(r, &slot33, &rect33);
+              rect33       = slot;
+              rect33.x     = workarea.x + props[type].grav_right * width66;
+              rect33.width = width33;
 
-              slot66       = desiredSlot;
-              slot66.x     = workarea.x + props.grav_right * slotWidth33;
-              slot66.width = slotWidth66;
-              GravityConstrainSize(r, &slot66, &rect66);
+              rect66       = slot;
+              rect66.x     = workarea.x + props[type].grav_right * width33;
+              rect66.width = width66;
 
-              if(currentRect.width == rect66.width && currentRect.x == rect66.x)
+              if(current.width == rect66.width && current.x == rect66.x)
                 {
-                  desiredSlot.width = slotWidth33;
-                  desiredSlot.x = workarea.x + props.grav_right * slotWidth66;
+                  slot.width = width33;
+                  slot.x     = workarea.x + props[type].grav_right * width66;
 	             }
 	         }
 	      }
 	    else
 	      {
-          if(currentRect.width == desiredRect.width && currentRect.x == desiredRect.x)
+          if(current.width == desired.width && current.x == desired.x)
             {
-              desiredSlot.width = slotWidth33;
-              desiredSlot.x     = workarea.x + slotWidth33;
+              slot.x     = workarea.x + width33;
+              slot.width = width33 + comp;
             }
         }
 
-      GravityConstrainSize(r, &desiredSlot, &desiredRect);
+      desired = slot;
     }
 
-  r->x      = desiredRect.x;
-  r->y      = desiredRect.y;
-  r->width  = desiredRect.width;
-  r->height = desiredRect.height;
+  r->x      = desired.x;
+  r->y      = desired.y;
+  r->width  = desired.width;
+  r->height = desired.height;
 } /* }}} */
