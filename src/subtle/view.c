6ef3e1644d13cd81df2f4cfac3b84df2c13db29d
unexist <none@none>
2009-04-30 21:53:13 +0200

    Added fix to avoid grab error after last client exited
    --HG--
    branch : trunk

--- src/subtle/view.c
+++ src/subtle/view.c
@@ -73,7 +73,7 @@ subViewConfigure(SubView *v)
 
   if(0 < subtle->clients->ndata)
     {
-      int i;
+      int i, total = 0;
       long vid = subArrayIndex(subtle->views, (void *)v);
 
       /* Clients */
@@ -101,9 +101,12 @@ subViewConfigure(SubView *v)
                   subEwmhSetCardinals(c->win, SUB_EWMH_NET_WM_DESKTOP, &vid, 1);
                 }
               else XMapRaised(subtle->disp, c->win);
+
+              total++;
             }
           else XUnmapWindow(subtle->disp, c->win); ///< Unmap other windows
         }
+      if(0 == total) subGrabSet(ROOT); ///< Set grabs
     }
 } /* }}} */
 
