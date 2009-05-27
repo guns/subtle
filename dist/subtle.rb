2a39d64e2d9c26d073d3224b33899b127f296845
unexist <none@none>
2009-05-27 23:24:04 +0200

    Updated default config
    --HG--
    branch : trunk

--- dist/subtle.rb
+++ dist/subtle.rb
@@ -49,37 +49,47 @@ COLORS = {
 # B5 = Button5
 #
 GRABS = {
-  "W-1"      => "ViewJump1",                  # Jump to view 1
-  "W-2"      => "ViewJump2",                  # Jump to view 2
-  "W-3"      => "ViewJump3",                  # Jump to view 3 
-  "W-4"      => "ViewJump4",                  # Jump to view 4
-  "W-B1"     => "WindowMove",                 # Move window
-  "W-B3"     => "WindowResize",               # Resize window
-  "W-f"      => "WindowFloat",                # Toggle float
-  "W-space"  => "WindowFull",                 # Toggle full
-  "W-s"      => "WindowStick",                # Toggle stick
-  "W-S-r"    => "WindowRaise",                # Raise window
-  "W-S-l"    => "WindowLower",                # Lower window
-  "W-h"      => "WindowLeft",                 # Select window left
-  "W-j"      => "WindowDown",                 # Select window below
-  "W-k"      => "WindowUp",                   # Select window above
-  "W-l"      => "WindowRight",                # Select window right
-  "W-S-k"    => "WindowKill",                 # Kill window
-  "S-KP_7"   => "GravityTopLeft",             # Set top left gravity
-  "S-KP_8"   => "GravityTop",                 # Set top gravity
-  "S-KP_9"   => "GravityTopRight",            # Set top right gravity
-  "S-KP_4"   => "GravityLeft",                # Set left gravity
-  "S-KP_5"   => "GravityCenter",              # Set center gravity
-  "S-KP_6"   => "GravityRight",               # Set right gravity
-  "S-KP_1"   => "GravityBottomLeft",          # Set bottom left gravity
-  "S-KP_2"   => "GravityBottom",              # Set bottom gravity
-  "S-KP_3"   => "GravityBottomRight",         # Set bottom right gravity
+  "W-1"      => "ViewJump1",                         # Jump to view 1
+  "W-2"      => "ViewJump2",                         # Jump to view 2
+  "W-3"      => "ViewJump3",                         # Jump to view 3 
+  "W-4"      => "ViewJump4",                         # Jump to view 4
+  "W-B1"     => "WindowMove",                        # Move window
+  "W-B3"     => "WindowResize",                      # Resize window
+  "W-f"      => "WindowFloat",                       # Toggle float
+  "W-space"  => "WindowFull",                        # Toggle full
+  "W-s"      => "WindowStick",                       # Toggle stick
+  "W-S-r"    => "WindowRaise",                       # Raise window
+  "W-S-l"    => "WindowLower",                       # Lower window
+  "W-Left"   => "WindowLeft",                        # Select window left
+  "W-Down"   => "WindowDown",                        # Select window below
+  "W-Up"      => "WindowUp",                         # Select window above
+  "W-Right"  => "WindowRight",                       # Select window right
+  "W-S-k"    => "WindowKill",                        # Kill window
+  "S-KP_7"   => "GravityTopLeft",                    # Set top left gravity
+  "S-KP_8"   => "GravityTop",                        # Set top gravity
+  "S-KP_9"   => "GravityTopRight",                   # Set top right gravity
+  "S-KP_4"   => "GravityLeft",                       # Set left gravity
+  "S-KP_5"   => "GravityCenter",                     # Set center gravity
+  "S-KP_6"   => "GravityRight",                      # Set right gravity
+  "S-KP_1"   => "GravityBottomLeft",                 # Set bottom left gravity
+  "S-KP_2"   => "GravityBottom",                     # Set bottom gravity
+  "S-KP_3"   => "GravityBottomRight",                # Set bottom right gravity
 
-  "A-Return" => "xterm",                      # Exec a term
+  "A-Return" => "xterm",                             # Exec a term
 
-  "S-F2"     => lambda { |c| puts c.name  },  # Print client name
-  "S-F3"     => lambda { puts version },      # Print subtlext version
-  "S-F4"     => lambda { |c|                  # Show client on view test
+  "C-KP_7"   => lambda { |c| c.gravity = horz(7) },  # Set top left gravity (horizontally)
+  "C-KP_8"   => lambda { |c| c.gravity = horz(8) },  # Set top gravity (horizontally)
+  "C-KP_9"   => lambda { |c| c.gravity = horz(9) },  # Set top right gravity (horizontally)
+  "C-KP_4"   => lambda { |c| c.gravity = horz(4) },  # Set left gravity (horizontally)
+  "C-KP_5"   => lambda { |c| c.gravity = horz(5) },  # Set center gravity (horizontally)
+  "C-KP_6"   => lambda { |c| c.gravity = horz(6) },  # Set right gravity (horizontally)
+  "C-KP_1"   => lambda { |c| c.gravity = horz(1) },  # Set bottom left gravity (horizontally)
+  "C-KP_2"   => lambda { |c| c.gravity = horz(2) },  # Set bottom gravity (horizontally)
+  "C-KP_3"   => lambda { |c| c.gravity = horz(3) },  # Set bottom right gravity (horizontally)
+
+  "S-F2"     => lambda { |c| puts c.name  },         # Print client name
+  "S-F3"     => lambda { puts version },             # Print subtlext version
+  "S-F4"     => lambda { |c|                         # Show client on view test
     v = add_view("test")
     t = add_tag("test")
 
