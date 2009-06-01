92c1db02254cccd209260047b065028aa657a994
unexist <none@none>
2009-06-01 20:48:14 +0200

    Updated default config
    --HG--
    branch : trunk

--- dist/subtle.rb
+++ dist/subtle.rb
@@ -50,58 +50,58 @@ COLORS = {
 #
 GRABS = {
   # View Jumps
-  "W-1"      => "ViewJump1",                         # Jump to view 1
-  "W-2"      => "ViewJump2",                         # Jump to view 2
-  "W-3"      => "ViewJump3",                         # Jump to view 3 
-  "W-4"      => "ViewJump4",                         # Jump to view 4
+  "W-1"      => "ViewJump1",                   # Jump to view 1
+  "W-2"      => "ViewJump2",                   # Jump to view 2
+  "W-3"      => "ViewJump3",                   # Jump to view 3 
+  "W-4"      => "ViewJump4",                   # Jump to view 4
 
   # Subtle grabs
-  "W-C-r"    => "SubtleReload",                      # Reload config
-  "W-C-q"    => "SubtleQuit",                        # Quit subtle
+  "W-C-r"    => "SubtleReload",                # Reload config
+  "W-C-q"    => "SubtleQuit",                  # Quit subtle
 
   # Window grabs
-  "W-B1"     => "WindowMove",                        # Move window
-  "W-B3"     => "WindowResize",                      # Resize window
-  "W-f"      => "WindowFloat",                       # Toggle float
-  "W-space"  => "WindowFull",                        # Toggle full
-  "W-s"      => "WindowStick",                       # Toggle stick
-  "W-r"      => "WindowRaise",                       # Raise window
-  "W-l"      => "WindowLower",                       # Lower window
-  "W-Left"   => "WindowLeft",                        # Select window left
-  "W-Down"   => "WindowDown",                        # Select window below
-  "W-Up"     => "WindowUp",                          # Select window above
-  "W-Right"  => "WindowRight",                       # Select window right
-  "W-S-k"    => "WindowKill",                        # Kill window
+  "W-B1"     => "WindowMove",                  # Move window
+  "W-B3"     => "WindowResize",                # Resize window
+  "W-f"      => "WindowFloat",                 # Toggle float
+  "W-space"  => "WindowFull",                  # Toggle full
+  "W-s"      => "WindowStick",                 # Toggle stick
+  "W-r"      => "WindowRaise",                 # Raise window
+  "W-l"      => "WindowLower",                 # Lower window
+  "W-Left"   => "WindowLeft",                  # Select window left
+  "W-Down"   => "WindowDown",                  # Select window below
+  "W-Up"     => "WindowUp",                    # Select window above
+  "W-Right"  => "WindowRight",                 # Select window right
+  "W-S-k"    => "WindowKill",                  # Kill window
 
   # Vertical gravities grabs
-  "W-KP_7"   => "GravityTopLeftVert",                # Set top left gravity
-  "W-KP_8"   => "GravityTopVert",                    # Set top gravity
-  "W-KP_9"   => "GravityTopRightVert",               # Set top right gravity
-  "W-KP_4"   => "GravityLeftVert",                   # Set left gravity
-  "W-KP_5"   => "GravityCenterVert",                 # Set center gravity
-  "W-KP_6"   => "GravityRightVert",                  # Set right gravity
-  "W-KP_1"   => "GravityBottomLeftVert",             # Set bottom left gravity
-  "W-KP_2"   => "GravityBottomVert",                 # Set bottom gravity
-  "W-KP_3"   => "GravityBottomRightVert",            # Set bottom right gravity
+  "W-KP_7"   => "GravityTopLeftVert",          # Set top left gravity
+  "W-KP_8"   => "GravityTopVert",              # Set top gravity
+  "W-KP_9"   => "GravityTopRightVert",         # Set top right gravity
+  "W-KP_4"   => "GravityLeftVert",             # Set left gravity
+  "W-KP_5"   => "GravityCenterVert",           # Set center gravity
+  "W-KP_6"   => "GravityRightVert",            # Set right gravity
+  "W-KP_1"   => "GravityBottomLeftVert",       # Set bottom left gravity
+  "W-KP_2"   => "GravityBottomVert",           # Set bottom gravity
+  "W-KP_3"   => "GravityBottomRightVert",      # Set bottom right gravity
 
   # Horizontal gravities grabs
-  "W-S-KP_7" => "GravityTopLeftHorz",                # Set top left gravity
-  "W-S-KP_8" => "GravityTopHorz",                    # Set top gravity
-  "W-S-KP_9" => "GravityTopRightHorz",               # Set top right gravity
-  "W-S-KP_4" => "GravityLeftHorz",                   # Set left gravity
-  "W-S-KP_5" => "GravityCenterHorz",                 # Set center gravity
-  "W-S-KP_6" => "GravityRightHorz",                  # Set right gravity
-  "W-S-KP_1" => "GravityBottomLeftHorz",             # Set bottom left gravity
-  "W-S-KP_2" => "GravityBottomHorz",                 # Set bottom gravity
-  "W-S-KP_3" => "GravityBottomRightHorz",            # Set bottom right gravity
+  "W-S-KP_7" => "GravityTopLeftHorz",          # Set top left gravity
+  "W-S-KP_8" => "GravityTopHorz",              # Set top gravity
+  "W-S-KP_9" => "GravityTopRightHorz",         # Set top right gravity
+  "W-S-KP_4" => "GravityLeftHorz",             # Set left gravity
+  "W-S-KP_5" => "GravityCenterHorz",           # Set center gravity
+  "W-S-KP_6" => "GravityRightHorz",            # Set right gravity
+  "W-S-KP_1" => "GravityBottomLeftHorz",       # Set bottom left gravity
+  "W-S-KP_2" => "GravityBottomHorz",           # Set bottom gravity
+  "W-S-KP_3" => "GravityBottomRightHorz",      # Set bottom right gravity
 
   # Exec
-  "A-Return" => "xterm",                             # Exec a term
+  "W-Return" => "xterm",                       # Exec a term
 
   # Ruby lambdas
-  "S-F2"     => lambda { |c| puts c.name  },         # Print client name
-  "S-F3"     => lambda { puts version },             # Print subtlext version
-  "S-F4"     => lambda { |c|                         # Show client on view test
+  "S-F2"     => lambda { |c| puts c.name  },   # Print client name
+  "S-F3"     => lambda { puts version },       # Print subtlext version
+  "S-F4"     => lambda { |c|                   # Show client on view test
     v = add_view("test")
     t = add_tag("test")
 
@@ -117,9 +117,9 @@ GRABS = {
 #
 TAGS = {
   "terms"   => "xterm|[u]?rxvt",
-  "browser" => "opera",
+  "browser" => "opera|firefox",
   "editor"  => "[g]?vim",
-  "stick"   => "mplayer",
+  "stick"   => "mplayer|imagemagick",
   "float"   => "mplayer|imagemagick|gimp"
 }  
 
