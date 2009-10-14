5050b269a1e89e4685efe03c1df89147fc182c91
unexist <none@none>
2009-10-14 23:18:02 +0200

    Added subGravityPublish() and subGravityDelMode()
    --HG--
    branch : trunk

--- src/subtle/gravity.c
+++ src/subtle/gravity.c
@@ -41,6 +41,8 @@ subGravityNew(void)
   SubGravity *g = GRAVITY(subSharedMemoryAlloc(1, sizeof(SubGravity)));
   g->flags |= SUB_TYPE_GRAVITY;
 
+  subSharedLogDebug("new=gravity\n");
+
   return g;
 } /* }}} */
 
@@ -65,11 +67,105 @@ subGravityAddMode(SubGravity *g,
   g->modes[g->nmodes].y      = MINMAX(g->modes[g->nmodes].y,      0, 100);
   g->modes[g->nmodes].width  = MINMAX(g->modes[g->nmodes].width,  0, 100);
   g->modes[g->nmodes].height = MINMAX(g->modes[g->nmodes].height, 0, 100);
- 
-  g->nmodes++;
 
-  printf("Mode: x=%d, y=%d, width=%d, height%d\n", 
-    mode->x, mode->y, mode->width, mode->height);
+  subSharedLogDebug("new=mode, x=%d, y=%d, width=%d, height%d\n", g->modes[g->nmodes].x, 
+    g->modes[g->nmodes].y, g->modes[g->nmodes].width, g->modes[g->nmodes].height);
+
+  g->nmodes++;
+} /* }}} */
+
+ /** subGravityDelMode {{{
+  * @brief Delete mode from gravtiy
+  * @param[in]  g   A #SubGravity
+  * @param[in]  id  Mode id
+  **/
+
+void
+subGravityDelMode(SubGravity *g,
+  int id)
+{
+  assert(g);
+
+  if(0 <= id && g->nmodes > id)
+    {
+      int i;
+
+      for(i = id; i < g->nmodes - 1; i++)
+        g->modes[i] = g->modes[i - 1];
+
+      (g->nmodes)--;
+      g->modes = (XRectangle *)subSharedMemoryRealloc(g->modes, (g->nmodes) * sizeof(XRectangle));
+
+      subSharedLogDebug("kill=mode, id=%d\n", id);
+    }
+
+} /* }}} */
+
+
+ /** subGravityPublish {{{
+  * @brief Publish gravities
+  **/
+
+void
+subGravityPublish(void)
+{
+  int i, j, pos = 0, len = 9;
+  char **modes = NULL, buf[30];
+  SubGravity *g = NULL;
+
+  assert(0 < subtle->gravities->ndata);
+
+  modes = (char **)subSharedMemoryAlloc(len, sizeof(char *));
+
+  for(i = 0; i < subtle->gravities->ndata; i++)
+    {
+      g = GRAVITY(subtle->gravities->data[i]);
+
+      for(j = 0; j < g->nmodes; j++)
+        {
+          /* Increase size of array */
+          if(pos == len)
+            {
+              len   *= 2;
+              modes  = (char **)subSharedMemoryRealloc(modes, len * sizeof(char *));
+            }
+          
+          /* Add mode to list */
+          snprintf(buf, sizeof(buf), "%d#%dx%d+%d+%d", i + 1, g->modes[j].x,  
+            g->modes[j].y, g->modes[j].width, g->modes[j].height);
+
+          modes[pos] = (char *)subSharedMemoryAlloc(strlen(buf) + 1, sizeof(char));
+          strncpy(modes[pos], buf, strlen(buf));
+          pos++;
+        }
+    }
+
+  /* EWMH: Gravity list */
+  subEwmhSetStrings(ROOT, SUB_EWMH_SUBTLE_GRAVITY_LIST, modes, pos);
+
+  subSharedLogDebug("publish=gravities, n=%d\n", pos);
+
+  /* Tidy up */
+  for(i = 0; i < pos; i++)
+    free(modes[i]);
+
+  free(modes);
+} /* }}} */
+
+ /** subGravityClear {{{
+  * @brief Delete all gravity modes
+  * @param[in]  g  A #SubGravity
+  **/
+
+void
+subGravityClear(SubGravity *g)
+{
+  assert(g);
+
+  free(g->modes);
+
+  g->modes  = NULL;
+  g->nmodes = 0;
 } /* }}} */
 
  /** subGravityKill {{{
@@ -81,6 +177,8 @@ void
 subGravityKill(SubGravity *g)
 {
   assert(g);
+
+  subGravityClear(g);
 
   free(g);
 } /* }}} */
