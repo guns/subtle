337407064e75b0f424700e9a60446c7e2ac4edc3
unexist <none@none>
2009-10-31 01:04:20 +0100

    Added new gravities to default config
    --HG--
    branch : trunk

--- dist/subtle.rb
+++ dist/subtle.rb
@@ -12,7 +12,7 @@ OPTIONS = {
   :border  => 2,                                   # Border size of the windows
   :step    => 5,                                   # Window move/resize key step
   :snap    => 10,                                  # Window border snapping
-  :gravity => 5,                                   # Default gravity (0 = copy gravity of focus)
+  :gravity => :center,                             # Default gravity (0 = copy gravity of focus)
   :urgent  => true,                                # Make transient windows urgent
   :resize  => false,                               # Respect resize hints in tile
   :padding => [ 0, 0, 0, 0 ],                      # Screen padding (left, right, top, bottom)
@@ -28,21 +28,21 @@ OPTIONS = {
 #
 PANEL = {
   :top       => [ :views, :title, :spacer, :tray, :sublets ],   # Top panel
-  :bottom    => [ ],                                              # Bottom panel
-  :stipple   => false,                                            # Add stipple to panels
-  :separator => "|"                                               # Separator between sublets
+  :bottom    => [ ],                                            # Bottom panel
+  :stipple   => false,                                          # Add stipple to panels
+  :separator => "|"                                             # Separator between sublets
 }
 
 #
 # Colors
 # 
 COLORS = { 
-  :fg_panel      => "#e2e2e5",  # Foreground color of bar
+  :fg_panel      => "#e2e2e5",  # Foreground color of panel
   :fg_views      => "#CF6171",  # Foreground color of view button
-  :fg_sublets    => "#000000",  # foreground color of sublets
+  :fg_sublets    => "#000000",  # Foreground color of sublets
   :fg_focus      => "#000000",  # Foreground color of focus window title and view
 
-  :bg_panel      => "#444444",  # Background color of bar
+  :bg_panel      => "#444444",  # Background color of panel
   :bg_views      => "#444444",  # Background color of view button
   :bg_sublets    => "#CF6171",  # Background color of sublets
   :bg_focus      => "#CF6171",  # Background color of focus window title and view
@@ -55,19 +55,37 @@ COLORS = {
 
 #
 # Gravities
-# 
+#
 GRAVITIES = {
-  :top_left     => [ [   0,   0,  50,  50 ], [   0,   0,  50,  66 ], [   0,   0,  50,  33 ] ],
-  :top          => [ [   0,   0, 100,  50 ], [   0,   0, 100,  66 ], [   0,   0, 100,  33 ] ],
-  :top_right    => [ [ 100,   0,  50,  50 ], [ 100,   0,  50,  66 ], [ 100,   0,  50,  33 ] ],
-  :left         => [ [   0,   0,  50, 100 ], [   0,  50,  50,  33 ], [   0,  50,  25,  33 ] ],
-  :center       => [ [   0,   0, 100, 100 ], [   0,  50, 100,  33 ], [  50,  50,  50,  33 ] ],
-  :right        => [ [ 100,   0,  50, 100 ], [ 100,  50,  50,  33 ], [ 100,  50,  25,  33 ] ],
-  :bottom_left  => [ [   0, 100,  50,  50 ], [   0, 100,  50,  66 ], [   0, 100,  50,  33 ] ],
-  :bottom       => [ [   0, 100, 100,  50 ], [   0, 100, 100,  66 ], [   0, 100, 100,  33 ] ],
-  :bottom_right => [ [ 100, 100,  50,  50 ], [ 100, 100,  50,  66 ], [ 100, 100,  50,  33 ] ]
-}
-  
+  :top_left       => [   0,   0,  50,  50 ], 
+  :top_left66     => [   0,   0,  50,  66 ],
+  :top_left33     => [   0,   0,  50,  33 ],
+  :top            => [   0,   0, 100,  50 ],
+  :top66          => [   0,   0, 100,  66 ],
+  :top33          => [   0,   0, 100,  33 ],
+  :top_right      => [ 100,   0,  50,  50 ],
+  :top_right66    => [ 100,   0,  50,  66 ],
+  :top_right33    => [ 100,   0,  50,  33 ],
+  :left           => [   0,   0,  50, 100 ],
+  :left66         => [   0,  50,  50,  33 ],
+  :left33         => [   0,  50,  25,  33 ],
+  :center         => [   0,   0, 100, 100 ],
+  :center66       => [   0,  50, 100,  33 ],
+  :center33       => [  50,  50,  50,  33 ],
+  :right          => [ 100,   0,  50, 100 ],
+  :right66        => [ 100,  50,  50,  33 ],
+  :right33        => [ 100,  50,  25,  33 ],
+  :bottom_left    => [   0, 100,  50,  50 ],
+  :bottom_left66  => [   0, 100,  50,  66 ],
+  :bottom_left33  => [   0, 100,  50,  33 ],
+  :bottom         => [   0, 100, 100,  50 ],
+  :bottom66       => [   0, 100, 100,  66 ],
+  :bottom33       => [   0, 100, 100,  33 ],
+  :bottom_right   => [ 100, 100,  50,  50 ],
+  :bottom_right66 => [ 100, 100,  50,  66 ],
+  :bottom_right33 => [ 100, 100,  50,  33 ]
+}  
+
 #
 # Dmenu settings
 #
@@ -110,9 +128,9 @@ GRABS = {
   # Window grabs
   "W-B1"     => :WindowMove,                  # Move window
   "W-B3"     => :WindowResize,                # Resize window
-  "W-f"      => :WindowFloat,                 # Toggle float
-  "W-space"  => :WindowFull,                  # Toggle full
-  "W-s"      => :WindowStick,                 # Toggle stick
+  "W-f"      => :WindowFloat,                 # Toggle float mode
+  "W-space"  => :WindowFull,                  # Toggle full mode
+  "W-s"      => :WindowStick,                 # Toggle stick mode
   "W-r"      => :WindowRaise,                 # Raise window
   "W-l"      => :WindowLower,                 # Lower window
   "W-Left"   => :WindowLeft,                  # Select window left
@@ -121,21 +139,21 @@ GRABS = {
   "W-Right"  => :WindowRight,                 # Select window right
   "W-S-k"    => :WindowKill,                  # Kill window
 
-  "A-S-1"    => :WindowScreen1,               # Set screen 1
-  "A-S-2"    => :WindowScreen2,               # Set screen 2
-  "A-S-3"    => :WindowScreen3,               # Set screen 3
-  "A-S-4"    => :WindowScreen4,               # Set screen 4
+  "A-S-1"    => :WindowScreen1,               # Move window to screen 1
+  "A-S-2"    => :WindowScreen2,               # Move window to screen 2
+  "A-S-3"    => :WindowScreen3,               # Move window to screen 3
+  "A-S-4"    => :WindowScreen4,               # Move window to screen 4
 
   # Gravities grabs
-  "W-KP_7"   => :GravityTopLeft,              # Set top left gravity
-  "W-KP_8"   => :GravityTop,                  # Set top gravity
-  "W-KP_9"   => :GravityTopRight,             # Set top right gravity
-  "W-KP_4"   => :GravityLeft,                 # Set left gravity
-  "W-KP_5"   => :GravityCenter,               # Set center gravity
-  "W-KP_6"   => :GravityRight,                # Set right gravity
-  "W-KP_1"   => :GravityBottomLeft,           # Set bottom left gravity
-  "W-KP_2"   => :GravityBottom,               # Set bottom gravity
-  "W-KP_3"   => :GravityBottomRight,          # Set bottom right gravity
+  "W-KP_7"    => [ :top_left,     :top_left66,     :top_left33     ],  # Top-left gravity
+  "W-KP_8"    => [ :top,          :top66,          :top33          ],  # Top gravity
+  "W-KP_9"    => [ :top_right,    :top_right66,    :top_right33    ],  # Top-right gravity
+  "W-KP_4"    => [ :left,         :left66,         :left33         ],  # Left gravity
+  "W-KP_5"    => [ :center,       :center66,       :center33       ],  # Center gravity
+  "W-KP_6"    => [ :right,        :right66,        :right33        ],  # Right gravity
+  "W-KP_1"    => [ :bottom_left,  :bottom_left66,  :bottom_left33  ],  # Bottom-left gravity
+  "W-KP_2"    => [ :bottom,       :bottom66,       :bottom33       ],  # Bottom gravity
+  "W-KP_3"    => [ :bottom_right, :bottom_right66, :bottom_right33 ],  # Bottom-right gravity
 
   # Exec
   "W-Return" => "xterm",                      # Exec a term
@@ -151,21 +169,21 @@ GRABS = {
 #
 TAGS = {
   "terms"   => "xterm|[u]?rxvt",
-  "browser" => "uzbl|opera|firefox",
-  "editor"  => "[g]?vim",
+  "browser" => "uzbl|opera|firefox|navigator",
+  "editor"  => { :regex => "[g]?vim", :resize => true },
   "stick"   => { :regex => "mplayer|imagemagick", :float => true, :stick => true },
   "float"   => { :regex => "gimp", :float => true },
-  "fixed"   => { :size => [ 10, 10, 100, 100 ], :stick => true }
+  "fixed"   => { :geometry => [ 10, 10, 100, 100 ], :stick => true }
 }  
 
 #
 # Views
 #
-VIEWS = [
-  { "terms" => "terms" },
-  { "www"   => "browser" },
-  { "dev"   => "editor|terms" }
-]
+VIEWS = {
+  "terms" => "terms",
+  "www"   => "browser|default",
+  "dev"   => "editor|terms"
+}
 
 #
 # Hooks
