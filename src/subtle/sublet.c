c6d26567728ed20c5e038f4e7eadf2274c2899c8
unexist <none@none>
2010-03-10 23:51:46 +0100

    Replaced sublet string parsing by SubText

--- src/subtle/sublet.c
+++ src/subtle/sublet.c
@@ -25,8 +25,8 @@ subSubletNew(void)
   /* Create new sublet */
   s = SUBLET(subSharedMemoryAlloc(1, sizeof(SubSublet)));
   s->flags = SUB_TYPE_SUBLET;
-  s->time  = subSharedTime();
-  s->text  = subArrayNew();
+  s->time  = subSubtleTime();
+  s->text  = subSharedTextNew();
   s->bg    = subtle->colors.bg_sublets;
 
   /* Create window */
@@ -60,6 +60,13 @@ subSubletUpdate(void)
     }
 } /* }}} */
 
+void
+subSubletSetData(SubSublet *s,
+  char *data)
+{
+  s->width = subSharedTextParse(subtle->dpy, subtle->font, s->text, data);
+}
+
  /** subSubletRender {{{
   * @brief Render sublets
   * @param[in]  s  A #SubSublet
@@ -68,39 +75,14 @@ subSubletUpdate(void)
 void
 subSubletRender(SubSublet *s)
 {
-  int j, width = 3;
-  SubText *t = NULL;
-
   assert(s);
 
   XSetWindowBackground(subtle->dpy, s->win, s->bg);
   XClearWindow(subtle->dpy, s->win);
 
   /* Render text parts */
-  for(j = 0; j < s->text->ndata; j++)
-    {
-      if((t = TEXT(s->text->data[j])) && t->flags & SUB_DATA_STRING) ///< Text
-        {
-          subSharedTextDraw(s->win, width, subtle->font.y, 
-            t->color, -1, t->data.string);
-
-          width += t->width;
-        }
-      else if(t->flags & SUB_DATA_NUM) ///< Icon
-        {
-          SubIcon *i = NULL;
-          int x = (0 == j) ? 0 : 2; ///< Add spacing when icon isn't first
-          
-          if((i = ICON(subtle->icons->data[t->data.num])))
-            {
-              subIconRender(i, s->win, width + x, abs(subtle->th - i->height) / 2, 
-                t->color, subtle->colors.bg_sublets);
-
-              /* Add spacing when isn't last */
-              width += i->width + x + (j != s->text->ndata - 1 ? 2 : 0); 
-            }
-        }
-    }
+  subSharedTextRender(subtle->dpy, subtle->gcs.font, subtle->font,
+    s->text, s->win, subtle->colors.fg_sublets, subtle->colors.bg_sublets);
 
   XResizeWindow(subtle->dpy, s->win, s->width, subtle->th);
   XSync(subtle->dpy, False); ///< Sync before going on
@@ -129,78 +111,6 @@ subSubletCompare(const void *a,
   if(!(s2->flags & (SUB_SUBLET_INTERVAL))) return -1;
 
   return s1->time < s2->time ? -1 : (s1->time == s2->time ? 0 : 1);
-} /* }}} */
-
- /** subSubletSetData {{{
-  * @brief Compare two sublets
-  * @param[in]  s     A #SubSublet
-  * @param[in]  data  Data
-  **/
-
-void
-subSubletSetData(SubSublet *s,
-  char *data)
-{
-  int i = 0, id = 0, left = 0, right = 0;
-  char *tok = NULL;
-  unsigned long color = 0;
-  SubText *t = NULL;
-
-  assert(s);
-
-  color    = subtle->colors.fg_sublets;
-  s->width = 6;
-
-  /* Split and iterate over tokens */
-  while((tok = strsep(&data, SEPARATOR)))
-    {
-      if('#' == *tok) color = atol(tok + 1); ///< Color
-      else if('\0' != *tok) ///< Text or icon
-        {
-          /* Recycle items */
-          if(i < s->text->ndata && (t = TEXT(s->text->data[i])))
-            {
-              if(t->flags & SUB_DATA_STRING && t->data.string)
-                free(t->data.string);
-            }
-          else if((t = TEXT(subSharedMemoryAlloc(1, sizeof(SubText)))))
-            subArrayPush(s->text, (void *)t);
-
-          /* Get icon from id */
-          if('!' == *tok && 0 <= (id = atoi(tok + 1)) && id <= subtle->icons->ndata)
-            {
-              SubIcon *p = ICON(subtle->icons->data[id]);
-
-              t->flags    = SUB_TYPE_TEXT|SUB_DATA_NUM;
-              t->data.num = id;
-              t->width    = p->width + (0 == i ? 2 : 4); ///< Add spacing and check if icon is first
-            }
-          else
-            {
-              t->flags       = SUB_TYPE_TEXT|SUB_DATA_STRING;
-              t->data.string = strdup(tok);
-              t->width       = subSharedTextWidth(tok, strlen(tok), &left, &right, False);
-
-              if(0 == i) t->width -= left; ///< Remove left bearing from first text item
-            }
-
-          t->color  = color;
-          s->width += t->width; ///< Add spacing
-          i++;
-        }
-    }
-  
-  /* Fix spacing of last item */
-  if(t && t->flags & SUB_DATA_STRING)
-    {
-      s->width -= right;
-      t->width -= right;
-    }
-  else if(t && t->flags & SUB_DATA_NUM)
-    {
-      s->width -= 2;
-      t->width -= 2;
-    }
 } /* }}} */
 
  /** subSubletPublish {{{
@@ -244,9 +154,6 @@ subSubletKill(SubSublet *s)
 {
   assert(s);
 
-  /* Call exit hook */
-
-
   /* Tidy up */
   subArrayRemove(subtle->panels, (void *)s);
   subHookRemove(s->instance, (void *)s);
@@ -264,7 +171,7 @@ subSubletKill(SubSublet *s)
   printf("Unloaded sublet (%s)\n", s->name);
 
   if(s->name) free(s->name);
-  subArrayKill(s->text, True);
+  subSharedTextFree(s->text);
   free(s);
 
   subSharedLogDebug("kill=sublet\n");
