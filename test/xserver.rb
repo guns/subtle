a9abbf6616995b4ce3882521a61b5a2b54be45c7
unexist <none@none>
2010-11-10 15:11:26 +0100

    Added Xvfb start script

---
+++ test/xserver.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/ruby
+#
+# @package test
+#
+# @file Start Xvfb server
+# @author Christoph Kappel <unexist@dorfelite.net>
+# @version $Id$
+#
+# This program can be distributed under the terms of the GNU GPL.
+# See the file COPYING.
+#
+
+require "mkmf"
+
+# Find Xvfb
+if((xvfb = find_executable0("Xvfb")).nil?)
+  raise "Xvfb not found in path"
+end
+
+system("#{xvfb} :10 -screen 0 1024x768x16 -I &> /dev/null")
+
+# vim:ts=2:bs=2:sw=2:et:fdm=marker
