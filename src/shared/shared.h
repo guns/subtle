69eb9b0c4d9a6d319e5046e382d2fcfee5067317
unexist <none@none>
2011-01-16 16:19:48 +0100

    Added SUB_EWMH_RESIZE flag

--- src/shared/shared.h
+++ src/shared/shared.h
@@ -63,6 +63,7 @@
 #define SUB_EWMH_FULL      (1L << 0)                              ///< EWMH full flag
 #define SUB_EWMH_FLOAT     (1L << 1)                              ///< EWMH float flag
 #define SUB_EWMH_STICK     (1L << 2)                              ///< EWMH stick flag
+#define SUB_EWMH_RESIZE    (1L << 3)                              ///< EWMH resize flag
 
 #define SUB_MATCH_NAME     (1L << 0)                              ///< Match SUBTLE_NAME
 #define SUB_MATCH_INSTANCE (1L << 1)                              ///< Match instance of WM_CLASS
